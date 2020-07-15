e69f6b60710722ebd05f4c43a1ba8dd05e4ac35a
Nicki Stone <nickik@amazon.com>
2020-07-15 13:45:25 -0700

    add do catch

--- Source/ElastiCurl.swift
+++ Source/ElastiCurl.swift
@@ -6,102 +6,106 @@ import Foundation
 import AwsCHttp
 
 func main() {
-    let allocator = TracingAllocator(tracingBytesOf: defaultAllocator)
+    do {
+        let allocator = TracingAllocator(tracingBytesOf: defaultAllocator)
 
-    AwsCommonRuntimeKit.initialize(allocator: allocator)
+        AwsCommonRuntimeKit.initialize(allocator: allocator)
 
-    let logger = Logger(pipe: stdout, level: LogLevel.trace, allocator: allocator)
+        let logger = Logger(pipe: stdout, level: LogLevel.trace, allocator: allocator)
 
-    // Pretend we get this from the CLI for now
+        // Pretend we get this from the CLI for now
 
-    let hostName = "www.amazon.com"
-    let port = UInt16(443)
+        let hostName = "www.amazon.com"
+        let port = UInt16(443)
 
-    // BUSINESS!
+        // BUSINESS!
 
-    let tlsContextOptions = TlsContextOptions(defaultClientWithAllocator: allocator)
-    try tlsContextOptions.setAlpnList("h2;http/1.1")
-    let tlsContext = try TlsContext(options: tlsContextOptions, mode: .client, allocator: allocator)
+        let tlsContextOptions = TlsContextOptions(defaultClientWithAllocator: allocator)
+        try tlsContextOptions.setAlpnList("h2;http/1.1")
+        let tlsContext = try TlsContext(options: tlsContextOptions, mode: .client, allocator: allocator)
 
-    let tlsConnectionOptions = tlsContext.newConnectionOptions()
-    let serverName = "www.amazon.com"
-    try tlsConnectionOptions.setServerName(serverName)
+        let tlsConnectionOptions = tlsContext.newConnectionOptions()
+        let serverName = "www.amazon.com"
+        try tlsConnectionOptions.setServerName(serverName)
 
-    let elg = try EventLoopGroup(threadCount: 1, allocator: allocator)
-    let hostResolver = try DefaultHostResolver(eventLoopGroup: elg, maxHosts: 8, maxTTL: 30, allocator: allocator)
-    let bootstrap = try ClientBootstrap(eventLoopGroup: elg, hostResolver: hostResolver, allocator: allocator)
+        let elg = try EventLoopGroup(threadCount: 1, allocator: allocator)
+        let hostResolver = try DefaultHostResolver(eventLoopGroup: elg, maxHosts: 8, maxTTL: 30, allocator: allocator)
+        let bootstrap = try ClientBootstrap(eventLoopGroup: elg, hostResolver: hostResolver, allocator: allocator)
 
-    var socketOptions = SocketOptions(socketType: .stream)
+        var socketOptions = SocketOptions(socketType: .stream)
 
-    let semaphore = DispatchSemaphore(value: 0)
+        let semaphore = DispatchSemaphore(value: 0)
 
-    var stream: HttpStream? = nil
-    var connection: HttpClientConnection? = nil
+        var stream: HttpStream? = nil
+        var connection: HttpClientConnection? = nil
 
-    var httpRequest: HttpRequest = HttpRequest(allocator: allocator)
-    httpRequest.method = "GET"
-    httpRequest.path = "/"
+        var httpRequest: HttpRequest = HttpRequest(allocator: allocator)
+        httpRequest.method = "GET"
+        httpRequest.path = "/"
 
-    //new header api
-    var headers = HttpHeaders(allocator: allocator)
-    if headers.add(name: "Host", value: hostName),
-        headers.add(name: "User-Agent", value: "Elasticurl"),
-        headers.add(name: "Accept", value: "*/*") {
-        
-        httpRequest.addHeaders(headers: headers)
-    }
+        //new header api
+        var headers = HttpHeaders(allocator: allocator)
+        if headers.add(name: "Host", value: hostName),
+           headers.add(name: "User-Agent", value: "Elasticurl"),
+           headers.add(name: "Accept", value: "*/*") {
 
-
-    let onIncomingHeaders: HttpRequestOptions.OnIncomingHeaders =
-    { stream, headerBlock, headers in
-        for header in headers {
-            if let name = header.name.toString(),
-                let value = header.name.toString() {
-                print(name + " : " + value)
-            }
+            httpRequest.addHeaders(headers: headers)
         }
+
+
+        let onIncomingHeaders: HttpRequestOptions.OnIncomingHeaders =
+                { stream, headerBlock, headers in
+                    for header in headers {
+                        if let name = header.name.toString(),
+                           let value = header.name.toString() {
+                            print(name + " : " + value)
+                        }
+                    }
+                }
+
+        let onBody: HttpRequestOptions.OnIncomingBody =
+                { stream, bodyChunk in
+                    let dataStr = String(decoding: bodyChunk, as: UTF8.self)
+                    print(dataStr)
+                }
+
+        let onBlockDone: HttpRequestOptions.OnIncomingHeadersBlockDone =
+                { stream, block in
+                }
+
+        let onComplete: HttpRequestOptions.OnStreamComplete =
+                { stream, errorCode in
+                }
+
+        var httpClientOptions = HttpClientConnectionOptions(clientBootstrap: bootstrap,
+                hostName: hostName,
+                initialWindowSize: Int.max,
+                port: port,
+                proxyOptions: nil,
+                socketOptions: socketOptions,
+                tlsOptions: tlsConnectionOptions,
+                onConnectionSetup: { (conn, errorCode) in
+                    if(errorCode != 0) {
+                        print("Connection Setup failed with code \(errorCode)")
+                        exit(-1)
+                    } else {
+                        print("Connection succeeded")
+                        connection = conn
+
+                        let requestOptions = HttpRequestOptions(request: httpRequest, onIncomingHeaders: onIncomingHeaders, onIncomingHeadersBlockDone: onBlockDone, onIncomingBody: onBody, onStreamComplete: onComplete)
+                        stream = connection!.newClientStream(requestOptions: requestOptions)
+                        stream!.activate()
+                    }
+                },
+                onConnectionShutdown: { (connection, errorCode) in
+                    semaphore.signal()
+                })
+
+        HttpClientConnection.createConnection(options: &httpClientOptions, allocator: allocator)
+        semaphore.wait()
+    } catch {
+
     }
-
-    let onBody: HttpRequestOptions.OnIncomingBody =
-    { stream, bodyChunk in
-        let dataStr = String(decoding: bodyChunk, as: UTF8.self)
-        print(dataStr)
-    }
-
-    let onBlockDone: HttpRequestOptions.OnIncomingHeadersBlockDone =
-    { stream, block in
-    }
-
-    let onComplete: HttpRequestOptions.OnStreamComplete =
-    { stream, errorCode in
-    }
-
-    var httpClientOptions = HttpClientConnectionOptions(clientBootstrap: bootstrap,
-                                                        hostName: hostName,
-                                                        initialWindowSize: Int.max,
-                                                        port: port,
-                                                        proxyOptions: nil,
-                                                        socketOptions: socketOptions,
-                                                        tlsOptions: tlsConnectionOptions,
-                                                        onConnectionSetup: { (conn, errorCode) in
-                                                            if (errorCode != 0) {
-                                                                print("Connection Setup failed with code \(errorCode)")
-                                                                exit(-1)
-                                                            } else {
-                                                                print("Connection succeeded")
-                                                                connection = conn
-                                                                
-                                                                let requestOptions = HttpRequestOptions(request: httpRequest, onIncomingHeaders: onIncomingHeaders, onIncomingHeadersBlockDone: onBlockDone, onIncomingBody: onBody, onStreamComplete: onComplete)
-                                                                stream = connection!.newClientStream(requestOptions: requestOptions)
-                                                                stream!.activate()
-                                                            }
-    },
-                                                        onConnectionShutdown: { (connection, errorCode) in
-                                                            semaphore.signal()
-    })
-
-    HttpClientConnection.createConnection(options: &httpClientOptions, allocator: allocator)
-    semaphore.wait()
 }
 
 
