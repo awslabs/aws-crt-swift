3000ca4a68c80c243ee7d89036af678b6c79e2d2
Nicki Stone <nickik@amazon.com>
2020-07-17 22:38:16 -0700

    removing empty space

--- Source/AwsCommonRuntimeKit/crt/CommandLine.swift
+++ Source/AwsCommonRuntimeKit/crt/CommandLine.swift
@@ -4,10 +4,6 @@
 import Foundation
 import AwsCCommon
 
-
-
-
-
 public struct CommandLineParser {
 	static func parseArguments(argc: Int32, arguments: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?> , optionString: String, options: [aws_cli_option]) -> [String: Any] {
         var argumentsDict = [String: Any] ()
