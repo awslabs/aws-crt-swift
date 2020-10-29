de2b558becb7bcecf5f155f5fa6649bb3e941bcf
Nicki Stone <nickik@amazon.com>
2020-10-29 15:18:41 -0700

    added ability to get headers from http message

--- Source/AwsCommonRuntimeKit/http/HttpMessage.swift
+++ Source/AwsCommonRuntimeKit/http/HttpMessage.swift
@@ -39,14 +39,26 @@ public class HttpMessage {
 //header handling
 public extension HttpMessage {
     var headerCount: Int {
-           return aws_http_message_get_header_count(self.rawValue)
+           return aws_http_message_get_header_count(rawValue)
+    }
+    
+    var headers: [HttpHeader] {
+        var headers = [HttpHeader]()
+        for index in 0...headerCount {
+            if let header = getHeader(atIndex: index) {
+                headers.append(header)
+            } else {
+                continue
+            }
+        }
+        return headers
     }
 
     func addHeaders(headers: HttpHeaders) {
         for index in 0...headerCount {
             var header = HttpHeader(name: "", value: "")
             if aws_http_headers_get_index(headers.rawValue, index, &header.rawValue) == AWS_OP_SUCCESS {
-                aws_http_message_add_header(self.rawValue, header.rawValue)
+                aws_http_message_add_header(rawValue, header.rawValue)
             } else {
                 continue
             }
@@ -54,7 +66,7 @@ public extension HttpMessage {
     }
 
     func removeHeader(atIndex index: Int) -> Bool {
-        if aws_http_message_erase_header(self.rawValue, index) != AWS_OP_SUCCESS {
+        if aws_http_message_erase_header(rawValue, index) != AWS_OP_SUCCESS {
             return false
         }
 
@@ -63,7 +75,7 @@ public extension HttpMessage {
 
     func getHeader(atIndex index: Int) -> HttpHeader? {
         var header = HttpHeader(name: "", value: "")
-        if aws_http_message_get_header(self.rawValue, &header.rawValue, index) != AWS_OP_SUCCESS {
+        if aws_http_message_get_header(rawValue, &header.rawValue, index) != AWS_OP_SUCCESS {
             return nil
         }
         return header
