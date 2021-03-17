d32681e8b7563d93d1a94cb9c2665720d04485be
Jonathan M. Henson <henso@amazon.com>
2021-03-17 10:35:28 -0700

    Typo fix.

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let s2nPackage = Package(
             name: "S2N",
             dependencies: targetCryptoDependencies,
             path: "aws-common-runtime/s2n",
-            exclude: ["bin", "cmake", "codebuild" "coverage", "docker-images", "docs", "lib", "libcrypto-build", "scram", "tests"],
+            exclude: ["bin", "cmake", "codebuild", "coverage", "docker-images", "docs", "lib", "libcrypto-build", "scram", "tests"],
             publicHeadersPath: "api",
         )
     ]
