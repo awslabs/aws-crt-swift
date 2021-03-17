c8bde7156bf2185aa7b7ea17eb50d1721ea04cf6
Jonathan M. Henson <henso@amazon.com>
2021-03-17 11:51:15 -0700

    here we go again

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let libCryptoPackage = Package(
         )
     ])
 
-packageDependencies.append(.package(libCryptoPackage))
+packageDependencies.append(libCryptoPackage)
 calDependencies.append("LibCrypto")
 ioDependencies.append("LibCrypto")
 
@@ -41,7 +41,7 @@ let s2nPackage = Package(
     targets: [
         .target(
             name: "S2N",
-            dependencies: ["LibCrypto"],
+            dependencies: [libCryptoPackage],
             path: "aws-common-runtime/s2n",
             exclude: ["bin", "cmake", "codebuild", "coverage", "docker-images", "docs", "lib", "libcrypto-build", "scram", "tests"],
             publicHeadersPath: "api"
@@ -49,7 +49,7 @@ let s2nPackage = Package(
     ]
 )
 
-packageDependencies.append(.package(s2nPackage))
+packageDependencies.append(s2nPackage)
 calDependencies.append("S2N")
 ioDependencies.append("S2N")
 #endif
