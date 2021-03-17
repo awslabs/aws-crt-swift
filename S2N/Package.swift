8e95d24715353209a2562e42e40ca5abaee8e257
Jonathan M. Henson <henso@amazon.com>
2021-03-17 13:45:10 -0700

    Move shit aorund.

--- S2N/Package.swift
+++ S2N/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .target(
             name: "S2N",
             dependencies: ["LibCrypto"],
-            path: "../aws-common-runtime/s2n",
+            path: "s2n",
             exclude: ["bin", "cmake", "codebuild", "coverage", "docker-images", "docs", "lib", "libcrypto-build", "scram", "tests"],
             publicHeadersPath: "api"
         )
