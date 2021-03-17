c9b404a905a6f952c9a80e252b8f9a4d226adff3
Jonathan M. Henson <henso@amazon.com>
2021-03-17 11:55:37 -0700

    here we go again

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let s2nPackage = Package(
     targets: [
         .target(
             name: "S2N",
-            dependencies: ['LibCrypto'],
+            dependencies: ["LibCrypto"],
             path: "aws-common-runtime/s2n",
             exclude: ["bin", "cmake", "codebuild", "coverage", "docker-images", "docs", "lib", "libcrypto-build", "scram", "tests"],
             publicHeadersPath: "api"
