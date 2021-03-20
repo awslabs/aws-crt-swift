284bde5bbe2f4e50779b2e576de1f55f5c56bcd7
Jonathan M. Henson <henso@amazon.com>
2021-03-20 07:29:26 +0000

    Ooooh we'll make it I swear, ooh oh, living on a prayer.

--- S2N/Package.swift
+++ S2N/Package.swift
@@ -15,8 +15,13 @@ let package = Package(
             name: "S2N",
             dependencies: ["LibCrypto"],
             path: "s2n",
-            exclude: ["bin", "cmake", "codebuild", "coverage", "docker-images", "docs", "lib", "libcrypto-build", "scram", "tests"],
-            publicHeadersPath: "api"
+            exclude: ["bin", "cmake", "codebuild", "coverage", "docker-images", "docs", "lib", "pq-crypto", "libcrypto-build", "scram", "tests"],
+            publicHeadersPath: "api",
+            cSettings: [
+                .headerSearchPath("./"),
+                .define("POSIX_C_SOURCE=200809L"),
+                .define("S2N_NO_PQ"),
+            ]
         )
     ]
 )
