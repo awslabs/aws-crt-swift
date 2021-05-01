c6cbd5917c236d2c41a7aeebd81dc12594a0d3c2
Justin Boswell <boswej@amazon.com>
2021-05-01 01:52:32 +0000

    Added logger to all tests, defaulted to trace for now

--- Test/AwsCommonRuntimeKitTests/CrtXCBaseTestCase.swift
+++ Test/AwsCommonRuntimeKitTests/CrtXCBaseTestCase.swift
@@ -5,19 +5,23 @@ import XCTest
 import AwsCommonRuntimeKit
 
 class CrtXCBaseTestCase: XCTestCase {
-    internal let allocator = TracingAllocator(tracingBytesOf: defaultAllocator)
+    internal let allocator = TracingAllocator(tracingStacksOf: defaultAllocator)
+    var logging: Logger?
 
     override func setUp() {
         super.setUp()
         AwsCommonRuntimeKit.initialize(allocator: self.allocator)
+        logging = Logger(pipe: stdout, level: .trace, allocator: defaultAllocator)
     }
 
     override func tearDown() {
         AwsCommonRuntimeKit.cleanUp()
         allocator.dump()
-
         XCTAssertEqual(allocator.count, 0,
                        "Memory was leaked: \(allocator.bytes) bytes in \(allocator.count) allocations")
+
+        logging = nil
+
         super.tearDown()
     }
 }
