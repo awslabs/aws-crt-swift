57ceefd88646417bca94a4f4e9bb0711a070274e
Justin Boswell <boswej@amazon.com>
2021-05-01 01:53:54 +0000

    All supported platforms have execinfo

--- aws-common-runtime/config/aws/common/config.h
+++ aws-common-runtime/config/aws/common/config.h
@@ -1,9 +1,9 @@
 #ifdef __APPLE__
-#include <TargetConditionals.h>
+#    include <TargetConditionals.h>
 #endif
 
 #ifndef AWS_COMMON_CONFIG_H
-#define AWS_COMMON_CONFIG_H
+#    define AWS_COMMON_CONFIG_H
 /**
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
@@ -14,32 +14,30 @@
  * should be considered to be an implementation detail, and can change at any
  * time.
  */
-#if TARGET_OS_IPHONE
-#define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#define AWS_HAVE_GCC_INLINE_ASM
+#    if TARGET_OS_IPHONE
+#        define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
+#        define AWS_HAVE_GCC_INLINE_ASM
 /* #undef AWS_HAVE_MSVC_MULX */
-#define AWS_HAVE_EXECINFO
-#define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#elif TARGET_OS_SIMULATOR
-#define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#define AWS_HAVE_GCC_INLINE_ASM
+#        define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
+#    elif TARGET_OS_SIMULATOR
+#        define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
+#        define AWS_HAVE_GCC_INLINE_ASM
 /* #undef AWS_HAVE_MSVC_MULX */
-#define AWS_HAVE_EXECINFO
-#define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#elif TARGET_OS_MAC
-#define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#define AWS_HAVE_GCC_INLINE_ASM
+#        define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
+#    elif TARGET_OS_MAC
+#        define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
+#        define AWS_HAVE_GCC_INLINE_ASM
 /* #undef AWS_HAVE_MSVC_MULX */
-#define AWS_HAVE_EXECINFO
-#define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#endif
+#        define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
+#    endif
 #else
-#define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#define AWS_HAVE_GCC_INLINE_ASM
+#    define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
+#    define AWS_HAVE_GCC_INLINE_ASM
 /* #undef AWS_HAVE_MSVC_MULX */
-#define AWS_HAVE_EXECINFO
-#define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
+#    define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
 #endif
 #define AWS_UNSTABLE_TESTING_API 1
 
 #define AWS_AFFINITY_METHOD 0
+
+#define AWS_HAVE_EXECINFO 1
