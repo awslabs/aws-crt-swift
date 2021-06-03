df8f1cf21b62cd7c52cba6eae589bb8e5f258865
Nicki Stone <nickik@amazon.com>
2021-06-03 09:13:28 -0700

    update all packages

--- Source/AwsCommonRuntimeKit/io/retryer/CRTAWSRetryStrategy.swift
+++ Source/AwsCommonRuntimeKit/io/retryer/CRTAWSRetryStrategy.swift
@@ -4,5 +4,19 @@
 import AwsCIo
 
 public final class CRTAWSRetryStrategy {
+    let allocator: Allocator
+
+    var rawValue: UnsafeMutablePointer<aws_retry_strategy>
     
+    init(retryStrategy: UnsafeMutablePointer<aws_retry_strategy>,
+         allocator: Allocator) {
+        self.rawValue = retryStrategy
+        self.allocator = allocator
+    }
+    
+    public convenience init(fromProvider impl: CRTRetryStrategy,
+                            allocator: Allocator = defaultAllocator) {
+        let wrapped = WrappedCRTRetryStrategy(impl: impl, allocator: allocator)
+        self.init(retryStrategy: &wrapped.rawValue, allocator: wrapped.allocator)
+    }
 }
