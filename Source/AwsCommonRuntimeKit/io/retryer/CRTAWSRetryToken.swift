9cc7177b2726b9a1958cdea0f11d0fb755f9054c
Nicki Stone <nickik@amazon.com>
2021-08-24 13:59:18 -0700

    remove init

--- Source/AwsCommonRuntimeKit/io/retryer/CRTAWSRetryToken.swift
+++ Source/AwsCommonRuntimeKit/io/retryer/CRTAWSRetryToken.swift
@@ -6,16 +6,6 @@ import AwsCIo
 public final class CRTAWSRetryToken {
     var rawValue: UnsafeMutablePointer<aws_retry_token>
 
-    public init(retryStrategy: CRTAWSRetryStrategy, allocator: Allocator = defaultAllocator) {
-        let atomicVar = Atomic<Int>(1)
-        let retryStrategyPtr: UnsafeMutablePointer<CRTAWSRetryStrategy> = fromPointer(ptr: retryStrategy)
-        let retryToken = aws_retry_token(allocator: allocator.rawValue,
-                                         retry_strategy: retryStrategy.rawValue,
-                                         ref_count: atomicVar.rawValue,
-                                         impl: retryStrategyPtr)
-        self.rawValue = fromPointer(ptr: retryToken)
-    }
-
     public init(rawValue: UnsafeMutablePointer<aws_retry_token>,
                 allocator: Allocator = defaultAllocator) {
         self.rawValue = rawValue
