a27b9f66dcfa9ebc32e41e6ae19f4237b1150df1
Nicki Stone <nickik@amazon.com>
2021-09-15 13:55:18 -0700

    rename tuple function

--- Source/AwsCommonRuntimeKit/event-streams/CRTEventStreamHeader.swift
+++ Source/AwsCommonRuntimeKit/event-streams/CRTEventStreamHeader.swift
@@ -20,7 +20,7 @@ public struct CRTEventStreamHeader {
     init(name: String,
          value: String,
          type: EventStreamHeaderType = .boolTrue) {
-        let emptyTupleName = createEmptyTuple()
+        let emptyTupleName = createEmptyTuple126()
         let emptyTupleValue = createEmptyTuple16()
         let headerValue = aws_event_stream_header_value_pair.__Unnamed_union_header_value(static_val: emptyTupleValue)
         var headerValuePair = aws_event_stream_header_value_pair(header_name_len: UInt8(name.count), header_name: emptyTupleName, header_value_type: type.rawValue, header_value: headerValue, header_value_len: UInt16(value.count), value_owned: 1)
