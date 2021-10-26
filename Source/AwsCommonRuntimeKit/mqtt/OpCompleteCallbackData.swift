2256ccfa26b7b8dc444d330196b62b95af0a7064
Nicki Stone <nickik@amazon.com>
2021-10-26 13:31:44 -0700

    oops fix build error

--- Source/AwsCommonRuntimeKit/mqtt/OpCompleteCallbackData.swift
+++ Source/AwsCommonRuntimeKit/mqtt/OpCompleteCallbackData.swift
@@ -1,7 +1,7 @@
 //  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0.
 
-typealias OnOperationComplete = (MqttConnection, Int16, CRTError) -> Void
+public typealias OnOperationComplete = (MqttConnection, Int16, CRTError) -> Void
 
 struct OpCompleteCallbackData {
     let onOperationComplete: OnOperationComplete
