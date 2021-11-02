33b1ce0172ddeefdb037d3659cb140b70e1652a7
Nicki Stone <nickik@amazon.com>
2021-11-02 11:17:26 -0700

    build for watchos and tvos

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let excludesFromAll = ["tests", "cmake", "CONTRIBUTING.md",
 var packageTargets: [Target] = []
 
 var package = Package(name: "AwsCrt",
-    platforms: [.iOS(.v11), .macOS(.v10_14)],
+                      platforms: [.iOS(.v11), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
     products: [
       .library(name: "AwsCommonRuntimeKit", targets: ["AwsCommonRuntimeKit"]),
       .executable(name: "Elasticurl", targets: ["Elasticurl"])
