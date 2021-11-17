74bb2ef28f7ffb660729c1542e874bd4023ab8ab
Nicki Stone <nickik@amazon.com>
2021-11-17 15:19:43 -0800

    remove unnecessary script now

--- macos-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-swift build --build-tests --enable-index-store --configuration debug -Xlinker -rpath -Xlinker "$(xcode-select -p)/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift-5.5/macosx"
-swift test --skip-build --enable-index-store --configuration debug
-swift build -c release -Xlinker -rpath -Xlinker "$(xcode-select -p)/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift-5.5/macosx"
