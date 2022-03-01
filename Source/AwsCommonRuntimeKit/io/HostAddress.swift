041e96920439aa1623e406ca1a006390a893b754
Nicki Stone <nickik@amazon.com>
2022-03-01 12:31:53 -0800

    add host address struct to copy over fields from c

---
+++ Source/AwsCommonRuntimeKit/io/HostAddress.swift
@@ -0,0 +1,35 @@
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0.
+
+import AwsCIo
+
+public struct HostAddress {
+    let host: String?
+    let address: String?
+    let recordType: AddressRecordType
+    let expiry: UInt64
+    let useCount: Int
+    let connectionFailureCount: Int
+    let weight: UInt8
+    
+    init(hostAddress: aws_host_address) {
+        self.host = String(awsString: hostAddress.host)
+        self.address = String(awsString: hostAddress.address)
+        self.recordType = AddressRecordType(rawValue: hostAddress.record_type)
+        self.expiry = hostAddress.expiry
+        self.useCount = hostAddress.use_count
+        self.connectionFailureCount = hostAddress.connection_failure_count
+        self.weight = hostAddress.weight
+    }
+    
+    init() {
+        self.host = nil
+        self.address = nil
+        self.recordType = .typeA
+        self.expiry = 0
+        self.useCount = 0
+        self.connectionFailureCount = 0
+        self.weight = 0
+    }
+}
+
