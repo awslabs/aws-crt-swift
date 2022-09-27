6448cc6c95c8c4d49b589aa4de0dc40225855e54
Waqar Ahmed Khan <waahm7@gmail.com>
2022-09-27 14:35:10 -0700

    Fix extra changes in connection manager

--- Source/AwsCommonRuntimeKit/http/HttpClientConnectionManager.swift
+++ Source/AwsCommonRuntimeKit/http/HttpClientConnectionManager.swift
@@ -18,36 +18,34 @@ public class HttpClientConnectionManager {
         let shutDownPtr: UnsafeMutablePointer<ShutDownCallbackOptions>? = fromOptionalPointer(ptr: options.shutDownOptions)
         //Todo: add new http2 options as properties in HttpClientConnectionOptions
         var mgrOptions = aws_http_connection_manager_options(bootstrap: options.clientBootstrap.rawValue,
-                initial_window_size: options.initialWindowSize,
-                socket_options: options.socketOptions.rawValue,
-                tls_connection_options: options.tlsOptions?.rawValue,
-                http2_prior_knowledge: false,
-                monitoring_options: options.monitoringOptions?.rawValue,
-                host: options.hostName.awsByteCursor,
-                port: options.port,
-                initial_settings_array: nil,
-                num_initial_settings: 0,
-                max_closed_streams: 0,
-                http2_conn_manual_window_management: false,
-                proxy_options: options.proxyOptions?.rawValue,
-                proxy_ev_settings: options.proxyEnvSettings?.rawValue,
-                max_connections: options.maxConnections,
-                shutdown_complete_user_data: shutDownPtr,
-                shutdown_complete_callback: { (userData) in
-                    guard let userData = userData else {
-                        return
-                    }
+                                                             initial_window_size: options.initialWindowSize,
+                                                             socket_options: options.socketOptions.rawValue,
+                                                             tls_connection_options: options.tlsOptions?.rawValue,
+                                                             http2_prior_knowledge: false,
+                                                             monitoring_options: options.monitoringOptions?.rawValue,
+                                                             host: options.hostName.awsByteCursor,
+                                                             port: options.port,
+                                                             initial_settings_array: nil,
+                                                             num_initial_settings: 0,
+                                                             max_closed_streams: 0,
+                                                             http2_conn_manual_window_management: false,
+                                                             proxy_options: options.proxyOptions?.rawValue,
+                                                             proxy_ev_settings: options.proxyEnvSettings?.rawValue,
+                                                             max_connections: options.maxConnections,
+                                                             shutdown_complete_user_data: shutDownPtr,
+                                                             shutdown_complete_callback: { (userData) in
+                                                                guard let userData = userData else {
+                                                                    return
+                                                                }
 
-                    let callbackOptions = userData.assumingMemoryBound(
-                            to: ShutDownCallbackOptions.self)
-                    defer {
-                        callbackOptions.deinitializeAndDeallocate()
-                    }
-                    callbackOptions.pointee.shutDownCallback(
-                            callbackOptions.pointee.semaphore)
-                },
-                enable_read_back_pressure: options.enableManualWindowManagement,
-                max_connection_idle_in_milliseconds: options.maxConnectionIdleMs)
+                                                                let callbackOptions = userData.assumingMemoryBound(
+                                                                    to: ShutDownCallbackOptions.self)
+                                                                defer {callbackOptions.deinitializeAndDeallocate()}
+                                                                callbackOptions.pointee.shutDownCallback(
+                                                                    callbackOptions.pointee.semaphore)
+                                                            },
+                                                             enable_read_back_pressure: options.enableManualWindowManagement,
+                                                             max_connection_idle_in_milliseconds: options.maxConnectionIdleMs)
 
         self.manager = aws_http_connection_manager_new(allocator.rawValue, &mgrOptions)
     }
