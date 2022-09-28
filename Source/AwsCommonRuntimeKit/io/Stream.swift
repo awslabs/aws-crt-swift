56dc45e233b1fac0270fc60a7b16ac6e3a14a890
Michael Graeb <graebm@amazon.com>
2022-09-28 10:46:00 -0700

    Fix acquire/release with AwsInputStream

--- Source/AwsCommonRuntimeKit/io/Stream.swift
+++ Source/AwsCommonRuntimeKit/io/Stream.swift
@@ -13,21 +13,16 @@ private var vtable = aws_input_stream_vtable(seek: doSeek,
         acquire: doAcquire,
         release: doRelease)
 
-// We need to wrap AWSStream protocol in a class so that we can utilize unmanaged reference counting in AWSInputStream
-private class AWSStreamClass {
-    public let awsStream: AwsStream
-    init(_ awsStream: AwsStream) {
-        self.awsStream = awsStream
-    }
-}
 public class AwsInputStream {
     var rawValue: aws_input_stream
-    private let awsStream: AWSStreamClass
+    let awsStream: AwsStream
     public var length: Int64
     public init(_ impl: AwsStream, allocator: Allocator = defaultAllocator) {
         length = Int64(impl.length)
-        awsStream = AWSStreamClass(impl)
-        rawValue = aws_input_stream(impl: Unmanaged<AWSStreamClass>.passRetained(awsStream).toOpaque(), vtable: &vtable, ref_count: aws_ref_count())
+        awsStream = impl
+        rawValue = aws_input_stream()
+        rawValue.vtable = UnsafePointer<aws_input_stream_vtable>(&vtable)
+        rawValue.impl = Unmanaged<AwsInputStream>.passUnretained(self).toOpaque()
     }
 }
 
@@ -81,8 +76,8 @@ extension FileHandle: AwsStream {
 private func doSeek(_ stream: UnsafeMutablePointer<aws_input_stream>!,
                     _ offset: Int64,
                     _ seekBasis: aws_stream_seek_basis) -> Int32 {
-    let inputStream = Unmanaged<AWSStreamClass>.fromOpaque(stream.pointee.impl).takeUnretainedValue().awsStream
-    if inputStream.seek(offset: offset, basis: seekBasis) {
+    let inputStream = Unmanaged<AwsInputStream>.fromOpaque(stream.pointee.impl).takeUnretainedValue()
+    if inputStream.awsStream.seek(offset: offset, basis: seekBasis) {
         return AWS_OP_SUCCESS
     }
     return AWS_OP_ERR
@@ -90,8 +85,8 @@ private func doSeek(_ stream: UnsafeMutablePointer<aws_input_stream>!,
 
 private func doRead(_ stream: UnsafeMutablePointer<aws_input_stream>!,
                     _ buffer: UnsafeMutablePointer<aws_byte_buf>!) -> Int32 {
-    let inputStream = Unmanaged<AWSStreamClass>.fromOpaque(stream.pointee.impl).takeUnretainedValue().awsStream
-    if inputStream.read(buffer: &buffer.pointee) {
+    let inputStream = Unmanaged<AwsInputStream>.fromOpaque(stream.pointee.impl).takeUnretainedValue()
+    if inputStream.awsStream.read(buffer: &buffer.pointee) {
         return AWS_OP_SUCCESS
     }
     return AWS_OP_ERR
@@ -99,24 +94,24 @@ private func doRead(_ stream: UnsafeMutablePointer<aws_input_stream>!,
 
 private func doGetStatus(_ stream: UnsafeMutablePointer<aws_input_stream>!,
                          _ result: UnsafeMutablePointer<aws_stream_status>!) -> Int32 {
-    let inputStream = Unmanaged<AWSStreamClass>.fromOpaque(stream.pointee.impl).takeUnretainedValue().awsStream
-    result.pointee = inputStream.status
+    let inputStream = Unmanaged<AwsInputStream>.fromOpaque(stream.pointee.impl).takeUnretainedValue()
+    result.pointee = inputStream.awsStream.status
     return AWS_OP_SUCCESS
 }
 
 private func doGetLength(_ stream: UnsafeMutablePointer<aws_input_stream>!,
                          _ result: UnsafeMutablePointer<Int64>!) -> Int32 {
-    let inputStream = Unmanaged<AWSStreamClass>.fromOpaque(stream.pointee.impl).takeUnretainedValue().awsStream
-    let length = inputStream.length
-  
+    let inputStream = Unmanaged<AwsInputStream>.fromOpaque(stream.pointee.impl).takeUnretainedValue()
+    let length = inputStream.awsStream.length
+
     result.pointee = Int64(length)
     return AWS_OP_SUCCESS
 }
 
 private func doAcquire(_ stream: UnsafeMutablePointer<aws_input_stream>!) {
-    _ = Unmanaged<AWSStreamClass>.fromOpaque(stream.pointee.impl).retain()
+    _ = Unmanaged<AwsInputStream>.fromOpaque(stream.pointee.impl).retain()
 }
 
 private func doRelease(_ stream: UnsafeMutablePointer<aws_input_stream>!) {
-    _ = Unmanaged<AWSStreamClass>.fromOpaque(stream.pointee.impl).takeRetainedValue()
+    Unmanaged<AwsInputStream>.fromOpaque(stream.pointee.impl).release()
 }
