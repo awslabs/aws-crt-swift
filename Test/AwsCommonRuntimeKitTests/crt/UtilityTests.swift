11b85a1b6dc508b64a99aedb662ece6085395237
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-07 14:15:29 -0700

    Fix unit test

--- Test/AwsCommonRuntimeKitTests/crt/UtilityTests.swift
+++ Test/AwsCommonRuntimeKitTests/crt/UtilityTests.swift
@@ -8,7 +8,7 @@ import XCTest
  #endif
 @testable import AwsCommonRuntimeKit
 
-class UtilityTests: XCTestCase {
+class UtilityTests: CrtXCBaseTestCase {
     func testMd5() throws {
         let hello = "Hello"
         let md5 = hello.base64EncodedMD5()
