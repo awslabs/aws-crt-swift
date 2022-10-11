76c7138448169d1b6708aadcebb98d17d5bc1923
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-11 11:07:28 -0700

    Improve http test

--- Test/AwsCommonRuntimeKitTests/http/HttpTests.swift
+++ Test/AwsCommonRuntimeKitTests/http/HttpTests.swift
@@ -5,13 +5,14 @@ import XCTest
 import AwsCCommon
 
 class HttpTests: CrtXCBaseTestCase {
+    let semaphore = DispatchSemaphore(value: 0)
 
     func testGetHttpRequest() async throws{
         let result = await sendGetHttpRequest()
         XCTAssertEqual(result, AWS_OP_SUCCESS)
     }
 
-    func getHttpConnection(host: String, port: UInt16)  async throws -> HttpClientConnection {
+    func getHttpConnection(host: String, ssh: Bool)  async throws -> HttpClientConnection {
         let tlsContextOptions = TlsContextOptions(defaultClientWithAllocator: allocator)
         try tlsContextOptions.setAlpnList("h2;http/1.1")
         let tlsContext = try TlsContext(options: tlsContextOptions, mode: .client, allocator: allocator)
@@ -29,6 +30,7 @@ class HttpTests: CrtXCBaseTestCase {
                 allocator: allocator)
 
         let socketOptions = SocketOptions(socketType: .stream)
+        let port = ssh ? UInt16(443) : UInt16(80)
         let httpClientOptions = HttpClientConnectionOptions(clientBootstrap: bootstrap,
                 hostName: host,
                 initialWindowSize: Int.max,
@@ -47,6 +49,45 @@ class HttpTests: CrtXCBaseTestCase {
         return headers;
     }
 
+    func getHttpRequestOptions(method: String, path: String, host: String) throws -> HttpRequestOptions {
+
+        let httpRequest: HttpRequest = try HttpRequest(allocator: allocator)
+        httpRequest.method = method
+        httpRequest.path = path == "" ? "/" : path
+
+        httpRequest.addHeaders(headers: try getHeaders(host: host))
+        let onIncomingHeaders: HttpRequestOptions.OnIncomingHeaders = { stream, headerBlock, headers in
+            let allHeaders = headers.getAll()
+            for header in allHeaders {
+                print(header.name + " : " + header.value)
+            }
+        }
+
+        let onBody: HttpRequestOptions.OnIncomingBody = { stream, bodyChunk in
+            print("onBody: \(bodyChunk)")
+        }
+
+        let onBlockDone: HttpRequestOptions.OnIncomingHeadersBlockDone = { stream, block in
+            print("onBlockDone")
+        }
+
+        let onComplete: HttpRequestOptions.OnStreamComplete = { stream, error in
+            print("onComplete")
+            XCTAssertEqual(error.code, AWS_OP_SUCCESS)
+            XCTAssertEqual(try! stream.statusCode(), 200)
+            self.semaphore.signal()
+        }
+
+        let requestOptions = HttpRequestOptions(request: httpRequest,
+                onIncomingHeaders: onIncomingHeaders,
+                onIncomingHeadersBlockDone: onBlockDone,
+                onIncomingBody: onBody,
+                onStreamComplete: onComplete)
+
+
+        return requestOptions
+    }
+
     func sendGetHttpRequest() async -> Int32 {
         do {
             let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
@@ -54,65 +95,22 @@ class HttpTests: CrtXCBaseTestCase {
                 print("no proper host was parsed from the url. quitting.")
                 exit(EXIT_FAILURE)
             }
-            let port = UInt16(443)
 
-            let semaphore = DispatchSemaphore(value: 0)
+            let httpRequestOptions = try getHttpRequestOptions(method: "GET", path: url.path, host: host)
 
-            let httpRequest: HttpRequest = try HttpRequest(allocator: allocator)
-            httpRequest.method = "GET"
-            let path = url.path == "" ? "/" : url.path
-            httpRequest.path = path
+            let connection = try await getHttpConnection(host: host, ssh: true)
 
-            httpRequest.addHeaders(headers: try getHeaders(host: host))
-
-            let onIncomingHeaders: HttpRequestOptions.OnIncomingHeaders = { stream, headerBlock, headers in
-                let allHeaders = headers.getAll()
-                for header in allHeaders {
-                    print(header.name + " : " + header.value)
-
-                }
-            }
-
-            let onBody: HttpRequestOptions.OnIncomingBody = { stream, bodyChunk in
-                print("onBody: \(bodyChunk)")
-            }
-
-            let onBlockDone: HttpRequestOptions.OnIncomingHeadersBlockDone = { stream, block in
-
-            }
-
-            let onComplete: HttpRequestOptions.OnStreamComplete = { stream, error in
-                XCTAssertEqual(error.code, AWS_OP_SUCCESS)
-                XCTAssertEqual(try! stream.statusCode(), 200)
-                print(error.message)
-                semaphore.signal()
-            }
-
-            do {
-                let connection = try await getHttpConnection(host: host, port: port)
-                let requestOptions = HttpRequestOptions(request: httpRequest,
-                        onIncomingHeaders: onIncomingHeaders,
-                        onIncomingHeadersBlockDone: onBlockDone,
-                        onIncomingBody: onBody,
-                        onStreamComplete: onComplete)
-                let stream = try connection.makeRequest(requestOptions: requestOptions)
-                try stream.activate()
-//                //try connection?.close()
-//                let stream2 = try connection.makeRequest(requestOptions: requestOptions)
-//                try stream2.activate()
-            } catch {
-                print("connection has shut down with error: \(error.localizedDescription)" )
-                semaphore.signal()
-            }
-
+            let stream = try connection.makeRequest(requestOptions: httpRequestOptions)
+            try stream.activate()
             semaphore.wait()
-
+            let status_code = try stream.statusCode()
+            XCTAssertEqual(status_code, 200)
             return AWS_OP_SUCCESS
         } catch let err {
-
             print(err)
             return AWS_OP_ERR
         }
     }
+
 
 }
