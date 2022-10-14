8d070e99574cf8373bd698ad77e7b8d58178690c
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-14 10:32:44 -0700

    clean up

--- Test/AwsCommonRuntimeKitTests/auth/SigV4SigningTests.swift
+++ Test/AwsCommonRuntimeKitTests/auth/SigV4SigningTests.swift
@@ -72,8 +72,6 @@ class SigV4SigningTests: CrtXCBaseTestCase {
                                    region: "us-east-1",
                                    signedBodyValue: .empty,
                                    shouldSignHeader: shouldSignHeader)
-
-
         let signedRequest = try await signer.signRequest(request: request, config: config)
 
         XCTAssertNotNil(signedRequest)
