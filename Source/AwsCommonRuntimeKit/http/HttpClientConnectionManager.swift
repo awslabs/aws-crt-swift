9e69e503ea3bc73d4c6406ef1cc75bd20cb198cd
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-14 15:01:28 -0700

    fix connection manager unintended changes

--- Source/AwsCommonRuntimeKit/http/HttpClientConnectionManager.swift
+++ Source/AwsCommonRuntimeKit/http/HttpClientConnectionManager.swift
@@ -16,45 +16,43 @@ public class HttpClientConnectionManager {
         // Todo: fix shutdown options
         let shutDownPtr: UnsafeMutablePointer<ShutDownCallbackOptions>? = fromOptionalPointer(ptr: options.shutDownOptions)
 
-        self.manager = options.hostName.withByteCursor { hostName in
-
+        guard let manager = (options.hostName.withByteCursor { hostNameCursor in
             var mgrOptions = aws_http_connection_manager_options(bootstrap: options.clientBootstrap.rawValue,
-                    initial_window_size: options.initialWindowSize,
-                    socket_options: options.socketOptions.rawValue,
-                    tls_connection_options: options.tlsOptions?.rawValue,
-                    http2_prior_knowledge: false,
-                    monitoring_options: options.monitoringOptions?.rawValue,
-                    host: hostName,
-                    port: options.port,
-                    initial_settings_array: nil,
-                    num_initial_settings: 0,
-                    max_closed_streams: 0,
-                    http2_conn_manual_window_management: false,
-                    proxy_options: options.proxyOptions?.rawValue,
-                    proxy_ev_settings: options.proxyEnvSettings?.rawValue,
-                    max_connections: options.maxConnections,
-                    shutdown_complete_user_data: shutDownPtr,
-                    shutdown_complete_callback: { (userData) in
-                        guard let userData = userData else {
-                            return
-                        }
-
-                        let callbackOptions = userData.assumingMemoryBound(
-                                to: ShutDownCallbackOptions.self)
-                        defer {
-                            callbackOptions.deinitializeAndDeallocate()
-                        }
-                        callbackOptions.pointee.shutDownCallback(
-                                callbackOptions.pointee.semaphore)
-                    },
-                    enable_read_back_pressure: options.enableManualWindowManagement,
-                    max_connection_idle_in_milliseconds: options.maxConnectionIdleMs)
+                                                                 initial_window_size: options.initialWindowSize,
+                                                                 socket_options: options.socketOptions.rawValue,
+                                                                 tls_connection_options: options.tlsOptions?.rawValue,
+                                                                 http2_prior_knowledge: false,
+                                                                 monitoring_options: options.monitoringOptions?.rawValue,
+                                                                 host: hostNameCursor,
+                                                                 port: options.port,
+                                                                 initial_settings_array: nil,
+                                                                 num_initial_settings: 0,
+                                                                 max_closed_streams: 0,
+                                                                 http2_conn_manual_window_management: false,
+                                                                 proxy_options: options.proxyOptions?.rawValue,
+                                                                 proxy_ev_settings: options.proxyEnvSettings?.rawValue,
+                                                                 max_connections: options.maxConnections,
+                                                                 shutdown_complete_user_data: shutDownPtr,
+                                                                 shutdown_complete_callback: { (userData) in
+                                                                    guard let userData = userData else {
+                                                                        return
+                                                                    }
+                                                                    
+                                                                    let callbackOptions = userData.assumingMemoryBound(
+                                                                        to: ShutDownCallbackOptions.self)
+                                                                    defer {
+                                                                        callbackOptions.deinitializeAndDeallocate()
+                                                                    }
+                                                                    callbackOptions.pointee.shutDownCallback(
+                                                                        callbackOptions.pointee.semaphore)
+                                                                },
+                                                                 enable_read_back_pressure: options.enableManualWindowManagement,
+                                                                 max_connection_idle_in_milliseconds: options.maxConnectionIdleMs)
             return aws_http_connection_manager_new(allocator.rawValue, &mgrOptions)
-        }
-
-        if self.manager == nil {
+        }) else {
             throw CommonRunTimeError.crtError(.makeFromLastError())
         }
+        self.manager = manager
     }
 
     /// Acquires an `HttpClientConnection` asynchronously.
