3c62d9188f6829d877cc5c0b50de5b17a8a78292
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-19 10:37:26 -0700

    remove todo

--- Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderCachedConfig.swift
+++ Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderCachedConfig.swift
@@ -1,6 +1,5 @@
 //  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0.
-// TODO: do we need set here? How is this being used in the SDK?
 public protocol CRTCredentialsProviderCachedConfig {
     var shutdownCallback: ShutdownCallback? { get }
     var source: CRTAWSCredentialsProvider { get }
