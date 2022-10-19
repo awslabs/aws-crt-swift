4fc66c5f315ff90c38d18bfb880552a14117eb84
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-19 10:37:51 -0700

    Cleanup

--- Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderCachedConfig.swift
+++ Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderCachedConfig.swift
@@ -1,5 +1,6 @@
 //  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0.
+
 public protocol CRTCredentialsProviderCachedConfig {
     var shutdownCallback: ShutdownCallback? { get }
     var source: CRTAWSCredentialsProvider { get }
