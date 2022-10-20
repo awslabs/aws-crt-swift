b64f2c3ebd6f1bdd791c8cbbfe90c447077556b6
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-20 16:05:34 -0700

    add blank line

--- Source/AwsCommonRuntimeKit/http/HttpProxyOptions.swift
+++ Source/AwsCommonRuntimeKit/http/HttpProxyOptions.swift
@@ -2,6 +2,7 @@
 //  SPDX-License-Identifier: Apache-2.0.
 import AwsCHttp
 import AwsCCommon
+
 // Todo: try to find a better for a persistent byte cursor
 public class HttpProxyOptions {
     private let allocator: Allocator
