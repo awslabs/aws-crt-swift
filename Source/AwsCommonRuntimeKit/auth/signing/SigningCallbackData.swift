db6a6e8f33ea9072d26c7c877b31266d8fa655c8
Waqar Ahmed Khan <waahm7@gmail.com>
2022-10-31 08:30:34 -0700

    Fix lint

--- Source/AwsCommonRuntimeKit/auth/signing/SigningCallbackData.swift
+++ Source/AwsCommonRuntimeKit/auth/signing/SigningCallbackData.swift
@@ -1,7 +1,9 @@
 //  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0.
 import AwsCAuth
-
+//TODO: fix SigningCallbackData.
+// It should not have signable pointer if we can avoid it.
+// Try to get rid of unowned HttpRequest as it should stay alive until callback has fired.
 typealias SignedContinuation = CheckedContinuation<HttpRequest, Error>
 struct SigningCallbackData {
     let allocator: Allocator
@@ -10,9 +12,9 @@ struct SigningCallbackData {
     let signable: UnsafeMutablePointer<aws_signable>?
 
     init(allocator: Allocator = defaultAllocator,
-                request: HttpRequest,
-                signable: UnsafeMutablePointer<aws_signable>?,
-                continuation: SignedContinuation) {
+         request: HttpRequest,
+         signable: UnsafeMutablePointer<aws_signable>?,
+         continuation: SignedContinuation) {
         self.allocator = allocator
         self.request = request
         self.signable = signable
