1bd4297151444d870308be4542d3b03dd415d621
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-08 19:42:44 -0800

    Fix client bootstrap

--- Source/AwsCommonRuntimeKit/io/ClientBootstrap.swift
+++ Source/AwsCommonRuntimeKit/io/ClientBootstrap.swift
@@ -2,29 +2,26 @@
 //  SPDX-License-Identifier: Apache-2.0.
 import AwsCIo
 
-public final class ClientBootstrap {
+public class ClientBootstrap {
     let rawValue: UnsafeMutablePointer<aws_client_bootstrap>
-    var enableBlockingShutdown: Bool = false
 
-    public init(eventLoopGroup elg: EventLoopGroup,
-                hostResolver: HostResolver,
-                allocator: Allocator = defaultAllocator,
-                shutdownCallback: ShutdownCallback? = nil) throws {
-        let shutdownCallbackCore = ShutdownCallbackCore(shutdownCallback)
+    public init(clientBootstrapOptions: ClientBootstrapOptions, allocator: Allocator = defaultAllocator) throws {
+        let shutdownCallbackCore = ShutdownCallbackCore(clientBootstrapOptions.shutdownCallback)
         let shutdownOptions = shutdownCallbackCore.getRetainedShutdownOptions()
-        guard let rawValue = (hostResolver.config.withCPointer { hostResolverConfigPointer in
-            var options = aws_client_bootstrap_options()
-            options.event_loop_group = elg.rawValue
-            options.host_resolver = hostResolver.rawValue
-            options.host_resolution_config = hostResolverConfigPointer
-            options.on_shutdown_complete = shutdownOptions.shutdown_callback_fn
-            options.user_data = shutdownOptions.shutdown_callback_user_data
-            return aws_client_bootstrap_new(allocator.rawValue, &options)
+        guard let rawValue = (clientBootstrapOptions.withCPointer(shutdownOptions: shutdownOptions) { clientBootstrapOptionsPointer in
+            return aws_client_bootstrap_new(allocator.rawValue, clientBootstrapOptionsPointer)
         }) else {
             shutdownCallbackCore.release()
             throw CommonRunTimeError.crtError(.makeFromLastError())
         }
         self.rawValue = rawValue
+    }
+
+    public convenience init(eventLoopGroup: EventLoopGroup,
+                            hostResolver: HostResolver,
+                            shutdownCallback: ShutdownCallback? = nil,
+                            allocator: Allocator = defaultAllocator) throws {
+        try self.init(clientBootstrapOptions: ClientBootstrapOptions(eventLoopGroup: eventLoopGroup, hostResolver: hostResolver, shutdownCallback: shutdownCallback), allocator: allocator)
     }
 
     deinit {
