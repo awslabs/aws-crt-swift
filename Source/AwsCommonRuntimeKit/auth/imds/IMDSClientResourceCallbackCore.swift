0f00e7e63fba89d10fcb04f73bb87c5bc059def4
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-14 15:49:18 -0800

    Fix get resource callback

--- Source/AwsCommonRuntimeKit/auth/imds/IMDSClientResourceCallbackCore.swift
+++ Source/AwsCommonRuntimeKit/auth/imds/IMDSClientResourceCallbackCore.swift
@@ -1,8 +1,11 @@
 //  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0.
 
-typealias ResourceContinuation = CheckedContinuation<String?, Error>
-class IMDSResourceCallbackCore {
+import AwsCCommon
+import AwsCAuth
+
+typealias ResourceContinuation = CheckedContinuation<String, Error>
+class IMDSClientCore {
     let continuation: ResourceContinuation
 
     init(continuation: ResourceContinuation) {
@@ -10,17 +13,39 @@ class IMDSResourceCallbackCore {
     }
 
     private func getRetainedSelf() -> UnsafeMutableRawPointer {
-        return Unmanaged<HostResolverCore>.passRetained(self).toOpaque()
+        return Unmanaged<IMDSClientCore>.passRetained(self).toOpaque()
     }
 
-    public static func getRetainedResource(resourcePath: String, client: IMDSClient, continuation: ResourceContinuation) async throws -> String? {
-        let core = IMDSResourceCallbackCore(continuation: continuation)
+    public static func getRetainedResource(resourcePath: String, client: IMDSClient, continuation: ResourceContinuation) {
+        let core = IMDSClientCore(continuation: continuation)
         let retainedSelf = core.getRetainedSelf()
+        resourcePath.withByteCursor { resourcePathCursor in
+            if(aws_imds_client_get_resource_async(client.rawValue,
+                                                  resourcePathCursor,
+                                                  resourceCallback,
+                                                  retainedSelf)) != AWS_OP_SUCCESS {
 
+                core.release()
+                continuation.resume(throwing: CommonRunTimeError.crtError(.makeFromLastError()))
+            }
+        }
     }
-
 
     private func release() {
         Unmanaged.passUnretained(self).release()
     }
+}
+
+private func resourceCallback(_ byteBuf: UnsafePointer<aws_byte_buf>?,
+                              _ errorCode: Int32,
+                              _ userData: UnsafeMutableRawPointer!) {
+    let imdsClientCore = Unmanaged<IMDSClientCore>.fromOpaque(userData).takeRetainedValue()
+    if errorCode != AWS_OP_SUCCESS {
+        imdsClientCore.continuation.resume(throwing: CommonRunTimeError.crtError(CRTError(code: errorCode)))
+        return
+    }
+
+    // Success
+    //TODO: do we need to make a copy?
+    imdsClientCore.continuation.resume(returning: String(cString: byteBuf!.pointee.buffer))
 }
