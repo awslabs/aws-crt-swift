4dd62a20bd77af3c6974f35252e950d32e33a77b
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-14 09:25:43 -0800

    Fix host resolver

--- Source/AwsCommonRuntimeKit/io/HostResolveCore.swift
+++ Source/AwsCommonRuntimeKit/io/HostResolveCore.swift
@@ -7,14 +7,10 @@ typealias HostResolvedContinuation = CheckedContinuation<[HostAddress], Error>
 /// Core classes have manual memory management.
 /// You have to balance the retain & release calls in all cases to avoid leaking memory.
 class HostResolveCore {
-    let host: AWSString
-    let hostResolver: HostResolver
     let continuation: HostResolvedContinuation
 
-    init(hostResolver: HostResolver, host: String, continuation: HostResolvedContinuation, allocator: Allocator) {
-        self.host = AWSString(host, allocator: allocator)
+    init(_ continuation: HostResolvedContinuation) {
         self.continuation = continuation
-        self.hostResolver = hostResolver
     }
 
     private func getRetainedSelf() -> UnsafeMutableRawPointer {
@@ -23,17 +19,18 @@ class HostResolveCore {
 
     /// This function does a manual retain on HostResolverCore
     /// to keep it until until onHostResolved callback has fired which will do the release.
-    func retainedResolve() {
-        let retainedSelf = getRetainedSelf()
+    static func retainedResolve(hostResolver: HostResolver, host: String, continuation: HostResolvedContinuation, allocator: Allocator) {
+        let core = HostResolveCore(continuation)
+        let retainedSelf = core.getRetainedSelf()
         withUnsafePointer(to: hostResolver.getHostResolutionConfig()) { hostResolutionConfigPointer in
             if aws_host_resolver_resolve_host(hostResolver.rawValue,
-                    host.rawValue,
+                    AWSString(host, allocator: allocator).rawValue,
                     onHostResolved,
                     hostResolutionConfigPointer,
                     retainedSelf) != AWS_OP_SUCCESS {
                 //TODO: this is wrong. Sometimes it triggers the error callback and sometimes it doesn't.
                 // I have a fix in progress in aws-c-io
-                release()
+                core.release()
                 continuation.resume(throwing: CommonRunTimeError.crtError(CRTError.makeFromLastError()))
             }
         }
