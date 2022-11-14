7c68472379cfbbed7b269a654d5b6eea421f0f0a
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-14 11:27:05 -0800

    Fix overflow

--- Source/AwsCommonRuntimeKit/crt/ByteBuffer.swift
+++ Source/AwsCommonRuntimeKit/crt/ByteBuffer.swift
@@ -286,11 +286,12 @@ extension ByteBuffer: IStreamable {
     public func read(buffer: UnsafeMutablePointer<UInt8>, maxLength: Int) throws -> Int {
         //TODO: fix overflow
         let curMaxLength = array.count - self.currentIndex
-        let arrayEnd = min(self.currentIndex+curMaxLength, array.count)
+        let max = min(curMaxLength, maxLength)
+        let arrayEnd = self.currentIndex + max
         //let bufferCapacity = maxLength
         //let arrayEnd = (bufferCapacity + self.currentIndex) < array.count ? bufferCapacity + self.currentIndex : array.count
 
-        let data = Data(array[self.currentIndex..<(arrayEnd)])
+        let data = Data(array[self.currentIndex..<arrayEnd])
         if data.count > 0 {
             data.copyBytes(to: buffer, count: data.count)
             self.currentIndex = arrayEnd
