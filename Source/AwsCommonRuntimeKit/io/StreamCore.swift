9397be4cf22466eb82d3d623266311c534a1cf6e
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-17 09:22:09 -0800

    Catch fix

--- Source/AwsCommonRuntimeKit/io/StreamCore.swift
+++ Source/AwsCommonRuntimeKit/io/StreamCore.swift
@@ -45,11 +45,9 @@ private func doSeek(_ stream: UnsafeMutablePointer<aws_input_stream>!,
         try iStreamable.seek(offset: offset, streamSeekType: streamSeekType)
         iStreamCore.isEndOfStream = false
         return AWS_OP_SUCCESS
-    }
-    catch CommonRunTimeError.crtError(let crtError) {
+    } catch CommonRunTimeError.crtError(let crtError) {
         return aws_raise_error(crtError.code)
-    }
-    catch {
+    } catch {
         return aws_raise_error(Int32(AWS_IO_STREAM_SEEK_FAILED.rawValue))
     }
 }
