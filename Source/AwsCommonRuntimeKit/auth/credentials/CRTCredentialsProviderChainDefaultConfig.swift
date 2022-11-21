abd09102b333dd992ffb0e9b059202c0aedcbaeb
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-21 08:43:37 -0800

    Refactor Credentials Provider (#101)

--- Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderChainDefaultConfig.swift
+++
@@ -1,7 +0,0 @@
-//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0.
-
-public protocol CRTCredentialsProviderChainDefaultConfig {
-    var shutdownCallback: ShutdownCallback? { get }
-    var bootstrap: ClientBootstrap { get }
-}
--- Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderProfileOptions.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0.
-
-public protocol CRTCredentialsProviderProfileOptions {
-    var shutdownCallback: ShutdownCallback? { get }
-    var configFileNameOverride: String? { get }
-    var profileFileNameOverride: String? { get }
-    var credentialsFileNameOverride: String? { get }
-}
