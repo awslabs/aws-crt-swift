29268a6eeeffc1a404acba26ff524b32c0ec5eed
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-21 10:38:41 -0800

    Refactor sdkutils profile

--- Source/AwsCommonRuntimeKit/sdkutils/CRTAWSProfileProperty.swift
+++ Source/AwsCommonRuntimeKit/sdkutils/CRTAWSProfileProperty.swift
@@ -6,10 +6,11 @@ import AwsCSdkUtils
 public struct CRTAWSProfileProperty {
     let rawValue: OpaquePointer
 
-    public init(rawValue: OpaquePointer) {
+    init(rawValue: OpaquePointer) {
         self.rawValue = rawValue
     }
 
+    /// Returns the property's string value
     public var value: String? {
         guard let awsString = aws_profile_property_get_value(rawValue) else {
             return nil
@@ -17,6 +18,7 @@ public struct CRTAWSProfileProperty {
         return String(awsString: awsString)
     }
 
+    /// Returns the value of a sub property with the given name, if it exists, in the property
     public func getSubProperty(name: String, allocator: Allocator = defaultAllocator) -> String? {
         let awsString = AWSString(name, allocator: allocator)
         guard let stringPointer = aws_profile_property_get_sub_property(rawValue, awsString.rawValue) else {
@@ -25,6 +27,7 @@ public struct CRTAWSProfileProperty {
         return String(awsString: stringPointer)
     }
 
+    /// Returns how many sub properties the property holds
     public var subPropertyCount: Int {
         return aws_profile_property_get_sub_property_count(rawValue)
     }
