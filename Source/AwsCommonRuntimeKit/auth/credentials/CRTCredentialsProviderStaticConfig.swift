3dffed334ff5eeca603b1e6e346ed28c99a7ab5a
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-21 08:43:37 -0800

    Refactor Credentials Provider (#101)

--- Source/AwsCommonRuntimeKit/auth/credentials/CRTCredentialsProviderStaticConfig.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0.
-
-public protocol CRTCredentialsProviderStaticConfigOptions {
-    var accessKey: String { get }
-    var secret: String { get }
-    var sessionToken: String? { get }
-    var shutdownCallback: ShutdownCallback? { get }
-}
