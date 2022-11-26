2386f4540b9444f406768be6dc053ac8bd03f0a5
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-25 18:25:40 -0800

    Refactor sdkutils (#106)

--- Source/AwsCommonRuntimeKit/sdkutils/CRTAWSProfile.swift
+++ Source/AwsCommonRuntimeKit/sdkutils/CRTAWSProfile.swift
@@ -3,29 +3,32 @@
 
 import AwsCSdkUtils
 
-public struct CRTAWSProfile {
+public class CRTAWSProfile {
     let rawValue: OpaquePointer
+    // Keep a reference of collection to keep it alive
+    let collection: CRTAWSProfileCollection
 
-    public init(rawValue: OpaquePointer) {
+    init(rawValue: OpaquePointer, collection: CRTAWSProfileCollection) {
         self.rawValue = rawValue
+        self.collection = collection
     }
 
-    public var name: String? {
-        guard let string = aws_profile_get_name(rawValue) else {
-            return nil
-        }
-        return String(awsString: string)
+    /// Returns a reference to the name of the provided profile
+    public var name: String {
+        String(awsString: aws_profile_get_name(rawValue))!
     }
 
+    /// Retrieves a reference to a property with the specified name, if it exists, from a profile
     public func getProperty(name: String, allocator: Allocator = defaultAllocator) -> CRTAWSProfileProperty? {
         let nameAwsString = AWSString(name, allocator: allocator)
         guard let propPointer = aws_profile_get_property(rawValue, nameAwsString.rawValue) else {
             return nil
         }
-        return CRTAWSProfileProperty(rawValue: propPointer)
+        return CRTAWSProfileProperty(rawValue: propPointer, collection: collection)
     }
 
+    /// Returns how many properties a profile holds
     public var propertyCount: Int {
-       return aws_profile_get_property_count(rawValue)
+       aws_profile_get_property_count(rawValue)
     }
 }
--- Test/AwsCommonRuntimeKitTests/sdkutils/CRTAWSProfileCollectionTests.swift
+++ Test/AwsCommonRuntimeKitTests/sdkutils/CRTAWSProfileCollectionTests.swift
@@ -6,30 +6,73 @@ import Foundation
 @testable import AwsCommonRuntimeKit
 
 class CRTAWSProfileCollectionTests: CrtXCBaseTestCase {
-    func testGetProfileCollectionFromBufferConfig() {
+    func testGetPropertyFromBufferConfig() throws {
         let fakeConfig = "[default]\r\nregion=us-west-2".data(using: .utf8)!
         let buffer = ByteBuffer(data: fakeConfig)
-        let profileCollection = CRTAWSProfileCollection(fromBuffer: buffer, source: .config)
-        XCTAssertNotNil(profileCollection)
+        let profileCollection = try CRTAWSProfileCollection(fromBuffer: buffer, source: .config, allocator: allocator)
+        let profile = profileCollection.getProfile(name: "default", allocator: allocator)
+        let property = profile?.getProperty(name: "region", allocator: allocator)
+        XCTAssertEqual("us-west-2", property?.value)
     }
-    
-    func testGetProfileCollectionFromBufferCreds() {
+
+    func testGetPropertyFromBufferCreds() throws {
         let fakeCreds = "[default]\r\naws_access_key_id=AccessKey\r\naws_secret_access_key=Sekrit".data(using: .utf8)!
         let buffer = ByteBuffer(data: fakeCreds)
-        let profileCollection = CRTAWSProfileCollection(fromBuffer: buffer, source: .credentials)
-        XCTAssertNotNil(profileCollection)
+        let profileCollection = try CRTAWSProfileCollection(fromBuffer: buffer, source: .credentials, allocator: allocator)
+        let profile = profileCollection.getProfile(name: "default", allocator: allocator)!
+        let property = profile.getProperty(name: "aws_access_key_id", allocator: allocator)!
+        XCTAssertEqual("AccessKey", property.value)
     }
-    
-    func testGetProfileCollectionFromMerge() {
+
+    func testGetProfileCollectionFromMerge() throws {
         let fakeConfig = "[default]\r\nregion=us-west-2".data(using: .utf8)!
         let configBuffer = ByteBuffer(data: fakeConfig)
-        let profileCollectionConfig = CRTAWSProfileCollection(fromBuffer: configBuffer, source: .config)
-        
+        let profileCollectionConfig = try CRTAWSProfileCollection(fromBuffer: configBuffer, source: .config)
+
         let fakeCreds = "[default]\r\naws_access_key_id=AccessKey\r\naws_secret_access_key=Sekrit".data(using: .utf8)!
         let credBuffer = ByteBuffer(data: fakeCreds)
-        let profileCollectionCreds = CRTAWSProfileCollection(fromBuffer: credBuffer, source: .credentials)
-        
-        let mergedCollection = CRTAWSProfileCollection(configProfileCollection: profileCollectionConfig, credentialProfileCollection: profileCollectionCreds, source: .credentials)
-        XCTAssertNotNil(mergedCollection)
+        let profileCollectionCreds = try CRTAWSProfileCollection(fromBuffer: credBuffer, source: .credentials, allocator: allocator)
+
+        let mergedCollection = try CRTAWSProfileCollection(configProfileCollection: profileCollectionConfig, credentialProfileCollection: profileCollectionCreds)
+        let profile = mergedCollection.getProfile(name: "default", allocator: allocator)!
+
+        let accessKey = profile.getProperty(name: "aws_access_key_id")!
+        XCTAssertEqual("AccessKey", accessKey.value)
+        let region = profile.getProperty(name: "region", allocator: allocator)!
+        XCTAssertEqual("us-west-2", region.value)
+    }
+
+    func testGetPropertyFromConfigFile() throws {
+        let profileCollection = try CRTAWSProfileCollection(fromFile: Bundle.module.path(forResource: "example_profile", ofType: "txt")!, source: .credentials, allocator: allocator)
+        let profile = profileCollection.getProfile(name: "default", allocator: allocator)!
+        let property = profile.getProperty(name: "aws_access_key_id", allocator: allocator)!
+        XCTAssertEqual("default_access_key_id", property.value)
+
+        let s3Properties = profile.getProperty(name: "s3")!
+        let subPropertyValue = s3Properties.getSubProperty(name: "max_concurrent_requests")!
+        XCTAssertEqual("20", subPropertyValue)
+
+        let crtUserProfile = profileCollection.getProfile(name: "crt_user", allocator: allocator)!
+        let secretAccessKey = crtUserProfile.getProperty(name: "aws_secret_access_key")!
+        XCTAssertEqual("example_secret_access_key", secretAccessKey.value)
+    }
+
+    func testCollectionOutOfScope() throws {
+        var profile: CRTAWSProfile! = nil
+        var crtUserProfile: CRTAWSProfile! = nil
+        do{
+            let profileCollection = try CRTAWSProfileCollection(fromFile: Bundle.module.path(forResource: "example_profile", ofType: "txt")!, source: .credentials, allocator: allocator)
+            profile = profileCollection.getProfile(name: "default", allocator: allocator)!
+            crtUserProfile = profileCollection.getProfile(name: "crt_user", allocator: allocator)!
+        }
+        let property = profile.getProperty(name: "aws_access_key_id", allocator: allocator)!
+        XCTAssertEqual("default_access_key_id", property.value)
+
+        let s3Properties = profile.getProperty(name: "s3")!
+        let subPropertyValue = s3Properties.getSubProperty(name: "max_concurrent_requests")!
+        XCTAssertEqual("20", subPropertyValue)
+
+        let secretAccessKey = crtUserProfile.getProperty(name: "aws_secret_access_key")!
+        XCTAssertEqual("example_secret_access_key", secretAccessKey.value)
     }
 }
