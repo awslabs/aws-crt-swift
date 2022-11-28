b77d822f38768db1a204b21bf4241d325b55f6dc
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-27 19:31:57 -0800

    Add PUT test

--- Test/AwsCommonRuntimeKitTests/http/HttpTests.swift
+++ Test/AwsCommonRuntimeKitTests/http/HttpTests.swift
@@ -7,28 +7,41 @@ import AwsCCommon
 
 class HttpTests: CrtXCBaseTestCase {
     let semaphore = DispatchSemaphore(value: 0)
+    let TEST_DOC_LINE: String = """
+                                This is a sample to prove that http downloads and uploads work. 
+                                It doesn't really matter what's in here, 
+                                we mainly just need to verify the downloads and uploads work.
+                                """
 
     func testGetHttpRequest() async throws {
-        try await sendGetHttpRequest()
+        try await sendHttpRequest(method: "GET", endpoint: "httpbin.org", path: "/get")
+        try await sendHttpRequest(method: "GET", endpoint: "httpbin.org", path: "/delete", expectedStatus: 405)
+        try await sendHttpRequest(method: "GET", endpoint: "httpbin.org", path: "/get", ssh: false, port: 80)
+
     }
-    
-    func sendGetHttpRequest() async throws {
-        let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-        guard let host = url.host else {
-            print("no proper host was parsed from the url. quitting.")
-            exit(EXIT_FAILURE)
-        }
 
-        let httpRequestOptions = try getHttpRequestOptions(method: "GET", path: url.path, host: host)
+    func testPutHttpRequest() async throws {
+        try await sendHttpRequest(method: "PUT", endpoint: "httpbin.org", path: "/anything", requestBody: TEST_DOC_LINE)
+    }
 
-        let connectionManager = try await getHttpConnectionManager(host: host, ssh: true)
+
+    func sendHttpRequest(method: String,
+                         endpoint: String,
+                         path: String,
+                         requestBody: String = "",
+                         expectedStatus: Int = 200,
+                         ssh: Bool = true,
+                         port: Int = 443) async throws {
+        let httpRequestOptions = try getHttpRequestOptions(method: method, endpoint: endpoint, path: path, body: requestBody, expectedStatusCode: expectedStatus)
+
+        let connectionManager = try await getHttpConnectionManager(endpoint: endpoint, ssh: ssh, port: port)
         let connection = try await connectionManager.acquireConnection()
 
         let stream = try connection.makeRequest(requestOptions: httpRequestOptions)
         try stream.activate()
         semaphore.wait()
         let status_code = try stream.statusCode()
-        XCTAssertEqual(status_code, 200)
+        XCTAssertEqual(status_code, expectedStatus)
     }
 
     func testHttpStreamIsReleasedIfNotActivated() async throws {
@@ -39,8 +52,8 @@ class HttpTests: CrtXCBaseTestCase {
                 exit(EXIT_FAILURE)
             }
 
-            let httpRequestOptions = try getHttpRequestOptions(method: "GET", path: url.path, host: host)
-            let connectionManager = try await getHttpConnectionManager(host: host, ssh: true)
+            let httpRequestOptions = try getHttpRequestOptions(method: "GET", endpoint: host, path: url.path)
+            let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
             let connection = try await connectionManager.acquireConnection()
             _ = try connection.makeRequest(requestOptions: httpRequestOptions)
         } catch let err {
@@ -48,12 +61,12 @@ class HttpTests: CrtXCBaseTestCase {
         }
     }
 
-    func getHttpConnectionManager(host: String, ssh: Bool) async throws -> HttpClientConnectionManager {
+    func getHttpConnectionManager(endpoint: String, ssh: Bool, port: Int) async throws -> HttpClientConnectionManager {
         let tlsContextOptions = TlsContextOptions(allocator: allocator)
-        tlsContextOptions.setAlpnList(["h2","http/1.1"])
+        tlsContextOptions.setAlpnList(["http/1.1"])
         let tlsContext = try TlsContext(options: tlsContextOptions, mode: .client, allocator: allocator)
         var tlsConnectionOptions = TlsConnectionOptions(context: tlsContext, allocator: allocator)
-        tlsConnectionOptions.serverName = host
+        tlsConnectionOptions.serverName = endpoint
 
         let elg = try EventLoopGroup(threadCount: 1, allocator: allocator)
         let hostResolver = try HostResolver(eventLoopGroup: elg, maxHosts: 8, maxTTL: 30, allocator: allocator)
@@ -62,31 +75,38 @@ class HttpTests: CrtXCBaseTestCase {
                 allocator: allocator)
 
         let socketOptions = SocketOptions(socketType: .stream)
-        let port = ssh ? UInt16(443) : UInt16(80)
+
         let httpClientOptions = HttpClientConnectionOptions(clientBootstrap: bootstrap,
-                hostName: host,
+                hostName: endpoint,
                 initialWindowSize: Int.max,
-                port: port,
+                port: UInt16(port),
                 proxyOptions: nil,
                 socketOptions: socketOptions,
-                tlsOptions: tlsConnectionOptions,
+                tlsOptions: ssh ? tlsConnectionOptions : nil,
                 monitoringOptions: nil)
         return try HttpClientConnectionManager(options: httpClientOptions)
     }
 
-    func getHeaders(host: String) throws -> HttpHeaders {
-        let headers = try HttpHeaders(allocator: allocator)
-        XCTAssertTrue(headers.add(name: "Host", value: host))
-        return headers;
+    struct Response: Codable {
+        let data: String
     }
 
-    func getHttpRequestOptions(method: String, path: String, host: String) throws -> HttpRequestOptions {
+    func getHttpRequestOptions(method: String,
+                               endpoint: String,
+                               path: String,
+                               body: String = "",
+                               expectedStatusCode: Int = 200) throws -> HttpRequestOptions {
 
         let httpRequest: HttpRequest = try HttpRequest(allocator: allocator)
         httpRequest.method = method
         httpRequest.path = path == "" ? "/" : path
+        httpRequest.body = ByteBuffer(data: body.data(using: .utf8)!)
 
-        httpRequest.addHeaders(headers: try getHeaders(host: host))
+        let headers = try HttpHeaders(allocator: allocator)
+        XCTAssertTrue(headers.add(name: "Host", value: endpoint))
+        XCTAssertTrue(headers.add(name: "Content-Length", value: String(body.count)))
+        httpRequest.addHeaders(headers: headers)
+
         let onIncomingHeaders: HttpRequestOptions.OnIncomingHeaders = { stream, headerBlock, headers in
             let allHeaders = headers.getAll()
             for header in allHeaders {
@@ -96,6 +116,11 @@ class HttpTests: CrtXCBaseTestCase {
 
         let onBody: HttpRequestOptions.OnIncomingBody = { stream, bodyChunk in
             print("onBody: \(bodyChunk)")
+
+            if !body.isEmpty {
+                let response: Response = try! JSONDecoder().decode(Response.self, from: bodyChunk)
+                XCTAssertEqual(response.data, body)
+            }
         }
 
         let onBlockDone: HttpRequestOptions.OnIncomingHeadersBlockDone = { stream, block in
@@ -104,8 +129,9 @@ class HttpTests: CrtXCBaseTestCase {
 
         let onComplete: HttpRequestOptions.OnStreamComplete = { stream, error in
             print("onComplete")
+            XCTAssertNil(error)
             XCTAssertEqual(error, nil)
-            XCTAssertEqual(try! stream.statusCode(), 200)
+            XCTAssertEqual(try! stream.statusCode(), expectedStatusCode)
             self.semaphore.signal()
         }
 
