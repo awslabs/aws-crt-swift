9d65f3c9cd14a4d62ebf9ac0a6c44b681a981031
Waqar Ahmed Khan <waahm7@gmail.com>
2022-11-29 18:38:10 -0800

    Remove http

--- Source/AwsCommonRuntimeKit/AwsCommonRuntimeKit.swift
+++ Source/AwsCommonRuntimeKit/AwsCommonRuntimeKit.swift
@@ -5,13 +5,11 @@ public struct AwsCommonRuntimeKit {
   public static let version = "@AWS_CRT_SWIFT_VERSION@"
 
   public static func initialize(allocator: Allocator = defaultAllocator) {
-    aws_http_library_init(allocator.rawValue)
     aws_auth_library_init(allocator.rawValue)
   }
 
   public static func cleanUp() {
     aws_auth_library_clean_up()
-    aws_http_library_clean_up()
   }
 
   private init() {}
