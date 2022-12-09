640eb2824df0c5363f96971e31cd749d1033d8c9
Waqar Ahmed Khan <waahm7@gmail.com>
2022-12-09 11:06:52 -0800

    initial handler

---
+++ Source/AwsCommonRuntimeKit/event-stream/EventStreamChannelHandler.swift
@@ -0,0 +1,46 @@
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0.
+import AwsCEventStreams
+import AwsCIo
+
+public class EventStreamChannelHandler {
+
+    let rawValue: UnsafeMutablePointer<aws_channel_handler>
+    let allocator: Allocator
+    let onMessageReceivedCallback: (_ errorCode: Int) -> ()
+    public init(onMessageReceivedCallback: @escaping (_ errorCode: Int) -> (),
+                initialWindowSize: Int? = nil,
+                allocator: Allocator = defaultAllocator) throws {
+        self.allocator = allocator
+        self.onMessageReceivedCallback = onMessageReceivedCallback
+
+        var options = aws_event_stream_channel_handler_options()
+        options.on_message_received = onMessageReceived
+        if let initialWindowSize = initialWindowSize {
+            options.manual_window_management = true
+            options.initial_window_size = initialWindowSize
+        }
+
+        guard let rawValue = aws_event_stream_channel_handler_new(allocator.rawValue, &options) else {
+            throw CommonRunTimeError.crtError(.makeFromLastError())
+        }
+
+        self.rawValue = rawValue
+    }
+
+    public func sendMessage(data: Data) throws {
+
+    }
+
+    //TODO: what to do in deinit? What if this object goes out of scope and onMessageRecieved ie called?
+    deinit {
+       // allocator.release(rawValue)
+    }
+}
+
+private func onMessageReceived(
+        message: UnsafeMutablePointer<aws_event_stream_message>?,
+        errorCode: Int32,
+        userData: UnsafeMutableRawPointer!) {
+
+}
