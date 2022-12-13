5308f269de6cc8e899335e2f882b2d657d82ba8f
Waqar Ahmed Khan <waahm7@gmail.com>
2022-12-13 13:12:44 -0800

    fix host resolver formatting

--- Test/AwsCommonRuntimeKitTests/event-stream/EventStreamTests.swift
+++ Test/AwsCommonRuntimeKitTests/event-stream/EventStreamTests.swift
@@ -10,7 +10,6 @@ class EventStreamTests: XCBaseTestCase {
     func testEncodeDecodeHeaders() async throws {
         let headers = [
             EventStreamHeader(name: "boolTrue", value: .bool(value: true)),
-            EventStreamHeader(name: "boolFalse", value: .bool(value: false)),
             EventStreamHeader(name: "byte", value: .byte(value: 16)),
             EventStreamHeader(name: "int16", value: .int32(value: 16)),
             EventStreamHeader(name: "int32", value: .int32(value: 32)),
@@ -18,6 +17,7 @@ class EventStreamTests: XCBaseTestCase {
             EventStreamHeader(name: "byteBuf", value: .byteBuf(value: "data".data(using: .utf8)!)),
             EventStreamHeader(name: "host", value: .string(value: "aws-crt-test-stuff.s3.amazonaws.com")),
             EventStreamHeader(name: "host", value: .string(value: "aws-crt-test-stuff.s3.amazonaws.com")),
+            EventStreamHeader(name: "boolFalse", value: .bool(value: false)),
             EventStreamHeader(name: "timestamp", value: .timestamp(value: 10)),
             EventStreamHeader(name: "uuid", value: .uuid(value: UUID(uuidString: "63318232-1C63-4D04-9A0C-6907F347704E")!)),
         ]
