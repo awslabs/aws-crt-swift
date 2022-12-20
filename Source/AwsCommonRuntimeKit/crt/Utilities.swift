45ccc8de85add03e01f9c88b7885c47b00a9b0ac
Waqar Ahmed Khan <waahm7@gmail.com>
2022-12-20 15:40:49 -0800

    Update Source/AwsCommonRuntimeKit/crt/Utilities.swift
    Co-authored-by: Michael Graeb <graebm@amazon.com>

--- Source/AwsCommonRuntimeKit/crt/Utilities.swift
+++ Source/AwsCommonRuntimeKit/crt/Utilities.swift
@@ -95,7 +95,7 @@ extension Date {
         Int64((self.timeIntervalSince1970 * 1000.0).rounded())
     }
 
-    init(milliseconds: Int64) {
+    init(millisecondsSince1970: Int64) {
         self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
     }
 }
