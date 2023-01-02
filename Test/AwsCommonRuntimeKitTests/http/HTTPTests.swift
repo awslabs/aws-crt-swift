126c130c07441eae5dd830af826a6bf8ef84f247
Waqar Ahmed Khan <waahm7@gmail.com>
2023-01-02 14:01:36 -0800

    Update url

--- Test/AwsCommonRuntimeKitTests/http/HTTPTests.swift
+++ Test/AwsCommonRuntimeKitTests/http/HTTPTests.swift
@@ -8,6 +8,7 @@ import AwsCHttp
 
 class HTTPTests: HTTPClientTestFixture {
     let host = "httpbin.org"
+    let getPath = "/get"
     let TEST_DOC_LINE: String = """
                                 This is a sample to prove that http downloads and uploads work. 
                                 It doesn't really matter what's in here, 
@@ -16,21 +17,20 @@ class HTTPTests: HTTPClientTestFixture {
 
     func testGetHTTPSRequest() async throws {
         let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
-        _ = try await sendHttpRequest(method: "GET", endpoint: host, path: "/get", connectionManager: connectionManager)
+        _ = try await sendHttpRequest(method: "GET", endpoint: host, path: getPath, connectionManager: connectionManager)
         _ = try await sendHttpRequest(method: "GET", endpoint: host, path: "/delete", expectedStatus: 405, connectionManager: connectionManager)
-        //try await sendHttpRequest(method: "GET", endpoint: host, path: "/get", ssh: false, port: 80, connectionManager: connectionManager)
     }
 
     func testGetHTTPRequest() async throws {
         let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: false, port: 80)
-        _ = try await sendHttpRequest(method: "GET", endpoint: host, path: "/get", connectionManager: connectionManager)
+        _ = try await sendHttpRequest(method: "GET", endpoint: host, path: getPath, connectionManager: connectionManager)
     }
 
     func testPutHttpRequest() async throws {
         let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
         let response = try await sendHttpRequest(
                 method: "PUT",
-                endpoint: "httpbin.org",
+                endpoint: host,
                 path: "/anything",
                 requestBody: TEST_DOC_LINE,
                 connectionManager: connectionManager)
@@ -45,13 +45,7 @@ class HTTPTests: HTTPClientTestFixture {
 
     func testHTTPStreamIsReleasedIfNotActivated() async throws {
         do {
-            let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-            guard let host = url.host else {
-                print("no proper host was parsed from the url. quitting.")
-                exit(EXIT_FAILURE)
-            }
-
-            let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+            let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
             let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
             let connection = try await connectionManager.acquireConnection()
             _ = try connection.makeRequest(requestOptions: httpRequestOptions)
@@ -62,12 +56,7 @@ class HTTPTests: HTTPClientTestFixture {
 
     func testStreamLivesUntilComplete() async throws {
         do {
-            let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-            guard let host = url.host else {
-                print("no proper host was parsed from the url. quitting.")
-                exit(EXIT_FAILURE)
-            }
-            let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+            let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
             let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
             let connection = try await connectionManager.acquireConnection()
             let stream = try connection.makeRequest(requestOptions: httpRequestOptions)
@@ -77,35 +66,23 @@ class HTTPTests: HTTPClientTestFixture {
     }
 
     func testManagerLivesUntilComplete() async throws {
-        let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-        guard let host = url.host else {
-            print("no proper host was parsed from the url. quitting.")
-            exit(EXIT_FAILURE)
-        }
-
         var connection: HTTPClientConnection! = nil
         do {
             let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
             connection = try await connectionManager.acquireConnection()
         }
-        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
         let stream = try connection.makeRequest(requestOptions: httpRequestOptions)
         try stream.activate()
         semaphore.wait()
     }
 
     func testConnectionLivesUntilComplete() async throws {
-        let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-        guard let host = url.host else {
-            print("no proper host was parsed from the url. quitting.")
-            exit(EXIT_FAILURE)
-        }
-
         var stream: HTTPStream! = nil
         do {
             let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
             let connection = try await connectionManager.acquireConnection()
-            let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+            let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
             stream = try connection.makeRequest(requestOptions: httpRequestOptions)
         }
         try stream.activate()
@@ -113,41 +90,26 @@ class HTTPTests: HTTPClientTestFixture {
     }
 
     func testConnectionCloseThrow() async throws {
-        let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-        guard let host = url.host else {
-            print("no proper host was parsed from the url. quitting.")
-            exit(EXIT_FAILURE)
-        }
         let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
         let connection = try await connectionManager.acquireConnection()
         connection.close()
-        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
         XCTAssertThrowsError( _ = try connection.makeRequest(requestOptions: httpRequestOptions))
     }
 
     func testConnectionCloseActivateThrow() async throws {
-        let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-        guard let host = url.host else {
-            print("no proper host was parsed from the url. quitting.")
-            exit(EXIT_FAILURE)
-        }
         let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
         let connection = try await connectionManager.acquireConnection()
-        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
         let stream = try connection.makeRequest(requestOptions: httpRequestOptions)
         connection.close()
         XCTAssertThrowsError(try stream.activate())
     }
 
     func testConnectionCloseIsIdempotent() async throws {
-        let url = URL(string: "https://aws-crt-test-stuff.s3.amazonaws.com/http_test_doc.txt")!
-        guard let host = url.host else {
-            print("no proper host was parsed from the url. quitting.")
-            exit(EXIT_FAILURE)
-        }
         let connectionManager = try await getHttpConnectionManager(endpoint: host, ssh: true, port: 443)
         let connection = try await connectionManager.acquireConnection()
-        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: url.path)
+        let httpRequestOptions = try getHTTPRequestOptions(method: "GET", endpoint: host, path: getPath)
         let stream = try connection.makeRequest(requestOptions: httpRequestOptions)
         connection.close()
         connection.close()
