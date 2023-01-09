1fd0a613c1037b40ea202ab539512d03d049a34c
Waqar Ahmed Khan <waahm7@gmail.com>
2023-01-09 12:50:16 -0600

    Fix: Credential's Expiration time (#136)

--- Test/AwsCommonRuntimeKitTests/auth/CredentialsTests.swift
+++ Test/AwsCommonRuntimeKitTests/auth/CredentialsTests.swift
@@ -16,7 +16,26 @@ class CredentialsTests: XCBaseTestCase {
         XCTAssertEqual(accessKey, credentials.getAccessKey())
         XCTAssertEqual(secret, credentials.getSecret())
         XCTAssertEqual(sessionToken, credentials.getSessionToken())
-        XCTAssertEqual(UInt64(expiration.timeIntervalSince1970), UInt64(credentials.getExpiration().timeIntervalSince1970))
+        XCTAssertEqual(UInt64(expiration.timeIntervalSince1970), UInt64(credentials.getExpiration()!.timeIntervalSince1970))
+
+    }
+    
+    func testCreateAWSCredentialsInfinity() async throws {
+        let accessKey = "AccessKey"
+        let secret = "Secret"
+        let sessionToken = "Token"
+        let expiration = Date(timeIntervalSince1970: (Double) (UInt64.max))
+
+        let credentials = try Credentials(accessKey: accessKey, secret: secret, sessionToken: sessionToken, expiration: expiration)
+
+        XCTAssertEqual(accessKey, credentials.getAccessKey())
+        XCTAssertEqual(secret, credentials.getSecret())
+        XCTAssertEqual(sessionToken, credentials.getSessionToken())
+        XCTAssertNil(credentials.getExpiration())
+
+        let expiration2 = Date(timeIntervalSince1970: (Double) (UInt64.max)+10)
+        let credentials2 = try Credentials(accessKey: accessKey, secret: secret, sessionToken: sessionToken, expiration: expiration2)
+        XCTAssertNil(credentials2.getExpiration())
     }
 
     func testCreateAWSCredentialsWithoutSessionToken() async throws {
@@ -29,8 +48,20 @@ class CredentialsTests: XCBaseTestCase {
         XCTAssertEqual(accessKey, credentials.getAccessKey())
         XCTAssertEqual(secret, credentials.getSecret())
         XCTAssertEqual(credentials.getSessionToken(), nil)
-        XCTAssertEqual(UInt64(expiration.timeIntervalSince1970), UInt64(credentials.getExpiration().timeIntervalSince1970))
+        XCTAssertEqual(UInt64(expiration.timeIntervalSince1970), UInt64(credentials.getExpiration()!.timeIntervalSince1970))
 
+    }
+
+    func testCreateAWSCredentialsWithoutExpiration() async throws {
+        let accessKey = "AccessKey"
+        let secret = "Secret"
+
+        let credentials = try Credentials(accessKey: accessKey, secret: secret, sessionToken: nil)
+
+        XCTAssertEqual(accessKey, credentials.getAccessKey())
+        XCTAssertEqual(secret, credentials.getSecret())
+        XCTAssertEqual(credentials.getSessionToken(), nil)
+        XCTAssertNil(credentials.getExpiration())
     }
 
     func testCreateAWSCredentialsWithoutAccessKeyThrows() async {
