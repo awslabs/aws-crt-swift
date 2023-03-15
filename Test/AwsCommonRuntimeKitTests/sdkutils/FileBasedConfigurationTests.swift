6b4e287c4e7bf19063ad96a33b69ff332083d641
Waqar Ahmed Khan <waahm7@gmail.com>
2023-03-15 15:37:58 -0700

    refactor test

--- Test/AwsCommonRuntimeKitTests/sdkutils/FileBasedConfigurationTests.swift
+++ Test/AwsCommonRuntimeKitTests/sdkutils/FileBasedConfigurationTests.swift
@@ -12,40 +12,45 @@ class FileBasedConfigurationTests: XCBaseTestCase {
         let configPath = Bundle.module.path(forResource: "example_credentials", ofType: "txt")!
         let fileBasedConfiguration = try FileBasedConfiguration(configFilePath: profilePath, credentialsFilePath: configPath, allocator: allocator)
         XCTAssertNotNil(fileBasedConfiguration)
-        let profile = fileBasedConfiguration.getSection(name: "default", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
-        let property = profile.getProperty(name: "aws_access_key_id", allocator: allocator)!
+        let defaultSection = fileBasedConfiguration.getSection(name: "default", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
+        XCTAssertEqual(defaultSection.propertyCount, 3)
+        let property = defaultSection.getProperty(name: "aws_access_key_id", allocator: allocator)!
         XCTAssertEqual("accessKey", property.value)
 
-        let s3Properties = profile.getProperty(name: "s3")!
+        let s3Properties = defaultSection.getProperty(name: "s3")!
+        XCTAssertEqual(s3Properties.subPropertyCount, 1)
         let subPropertyValue = s3Properties.getSubProperty(name: "max_concurrent_requests")!
         XCTAssertEqual("20", subPropertyValue)
 
-        let crtUserProfile = fileBasedConfiguration.getSection(name: "crt_user", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
-        let secretAccessKey = crtUserProfile.getProperty(name: "aws_secret_access_key")!
+        let crtUserSection = fileBasedConfiguration.getSection(name: "crt_user", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
+        XCTAssertEqual(crtUserSection.propertyCount, 2)
+        let secretAccessKey = crtUserSection.getProperty(name: "aws_secret_access_key")!
         XCTAssertEqual("example_secret_access_key", secretAccessKey.value)
 
-        let credProfile = fileBasedConfiguration.getSection(name: "credentials", sectionType: FileBasedConfigSectionType.profile)!
-        XCTAssertEqual("accessKey1", credProfile.getProperty(name: "aws_access_key_id")?.value)
+        let credSection = fileBasedConfiguration.getSection(name: "credentials", sectionType: FileBasedConfigSectionType.profile)!
+        XCTAssertEqual("accessKey1", credSection.getProperty(name: "aws_access_key_id")?.value)
+        XCTAssertEqual(credSection.propertyCount, 2)
+
     }
 
     func testCollectionOutOfScope() throws {
-        var profile: FileBasedConfigurationSection! = nil
-        var crtUserProfile: FileBasedConfigurationSection! = nil
+        var defaultSection: FileBasedConfigurationSection! = nil
+        var crtUserSection: FileBasedConfigurationSection! = nil
         do{
             let profilePath = Bundle.module.path(forResource: "example_profile", ofType: "txt")!
             let configPath = Bundle.module.path(forResource: "example_credentials", ofType: "txt")!
             let fileBasedConfiguration = try FileBasedConfiguration(configFilePath: profilePath, credentialsFilePath: configPath, allocator: allocator)
-            profile = fileBasedConfiguration.getSection(name: "default", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
-            crtUserProfile = fileBasedConfiguration.getSection(name: "crt_user", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
+            defaultSection = fileBasedConfiguration.getSection(name: "default", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
+            crtUserSection = fileBasedConfiguration.getSection(name: "crt_user", sectionType: FileBasedConfigSectionType.profile, allocator: allocator)!
         }
-        let property = profile.getProperty(name: "aws_access_key_id", allocator: allocator)!
+        let property = defaultSection.getProperty(name: "aws_access_key_id", allocator: allocator)!
         XCTAssertEqual("accessKey", property.value)
 
-        let s3Properties = profile.getProperty(name: "s3")!
+        let s3Properties = defaultSection.getProperty(name: "s3")!
         let subPropertyValue = s3Properties.getSubProperty(name: "max_concurrent_requests")!
         XCTAssertEqual("20", subPropertyValue)
 
-        let secretAccessKey = crtUserProfile.getProperty(name: "aws_secret_access_key")!
+        let secretAccessKey = crtUserSection.getProperty(name: "aws_secret_access_key")!
         XCTAssertEqual("example_secret_access_key", secretAccessKey.value)
     }
 }
