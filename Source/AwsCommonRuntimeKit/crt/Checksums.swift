1abab2017bf23da14d4fbac20e0e96220315e09f
Waqar Ahmed Khan <waahm7@gmail.com>
2023-11-07 09:37:44 -0800

    remove the guard

--- Source/AwsCommonRuntimeKit/crt/Checksums.swift
+++ Source/AwsCommonRuntimeKit/crt/Checksums.swift
@@ -10,10 +10,7 @@ extension Data {
     /// - Parameter previousCrc32:  Pass 0 in the previousCrc32 parameter as an initial value unless continuing to update a running crc in a subsequent call.
     public func crc32(previousCrc32: UInt32 = 0) -> UInt32 {
         self.withUnsafeBytes { bufferPointer in
-            guard let ptr = bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
-                return 0
-            }
-            return aws_checksums_crc32(ptr, Int32(count), previousCrc32)
+            return aws_checksums_crc32(bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self), Int32(count), previousCrc32)
         }
     }
     
@@ -21,10 +18,7 @@ extension Data {
     /// - Parameter previousCrc32c:  Pass 0 in the previousCrc32c parameter as an initial value unless continuing to update a running crc in a subsequent call.
     public func crc32c(previousCrc32c: UInt32 = 0) -> UInt32 {
         self.withUnsafeBytes { bufferPointer in
-            guard let ptr = bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
-                return 0
-            }
-            return aws_checksums_crc32c(ptr, Int32(count), previousCrc32c)
+            return aws_checksums_crc32c(bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self), Int32(count), previousCrc32c)
          }
     }
     
