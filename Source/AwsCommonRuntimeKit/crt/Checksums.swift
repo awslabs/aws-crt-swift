60da2db9624e5eaa2986c1f83bacd5a0c347c476
Waqar Ahmed Khan <waahm7@gmail.com>
2023-11-07 09:39:47 -0800

    lint fix

--- Source/AwsCommonRuntimeKit/crt/Checksums.swift
+++ Source/AwsCommonRuntimeKit/crt/Checksums.swift
@@ -10,7 +10,9 @@ extension Data {
     /// - Parameter previousCrc32:  Pass 0 in the previousCrc32 parameter as an initial value unless continuing to update a running crc in a subsequent call.
     public func crc32(previousCrc32: UInt32 = 0) -> UInt32 {
         self.withUnsafeBytes { bufferPointer in
-            return aws_checksums_crc32(bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self), Int32(count), previousCrc32)
+            return aws_checksums_crc32(bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self),
+                                       Int32(count),
+                                       previousCrc32)
         }
     }
     
@@ -18,7 +20,9 @@ extension Data {
     /// - Parameter previousCrc32c:  Pass 0 in the previousCrc32c parameter as an initial value unless continuing to update a running crc in a subsequent call.
     public func crc32c(previousCrc32c: UInt32 = 0) -> UInt32 {
         self.withUnsafeBytes { bufferPointer in
-            return aws_checksums_crc32c(bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self), Int32(count), previousCrc32c)
+            return aws_checksums_crc32c(bufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self), 
+                                        Int32(count),
+                                        previousCrc32c)
          }
     }
     
