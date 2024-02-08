6560e7d9b2501e975b45605f08372d9276b513c7
Waqar Ahmed Khan <waahm7@gmail.com>
2024-02-08 09:16:37 -0800

    Remove swift-collections dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ var package = Package(name: "aws-crt-swift",
                         .library(name: "AwsCommonRuntimeKit", targets: ["AwsCommonRuntimeKit"]),
                         .executable(name: "Elasticurl", targets: ["Elasticurl"])
                       ],
-                      dependencies: [.package(url: "https://github.com/apple/swift-collections", from: "1.0.2")]
+                      dependencies: []
 )
 
 let cSettings: [CSetting] = [
@@ -266,8 +266,7 @@ packageTargets.append(contentsOf: [
                         "AwsCIo",
                         "AwsCCommon",
                         "AwsCChecksums",
-                        "AwsCEventStream",
-                        .product(name: "Collections", package: "swift-collections")],
+                        "AwsCEventStream"],
         path: "Source/AwsCommonRuntimeKit"
     ),
     .testTarget(
