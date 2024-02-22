7c955499d719af2ea651b8eea3b0af9f0c0212e7
Zhihui Xia <zhvxia@amazon.com>
2024-02-22 13:22:13 -0800

    test macos, and test with skipifios

--- Test/AwsCommonRuntimeKitTests/io/TLSContextTests.swift
+++ Test/AwsCommonRuntimeKitTests/io/TLSContextTests.swift
@@ -11,13 +11,12 @@ class TLSContextTests: XCBaseTestCase {
     _ = TLSConnectionOptions(context: context)
   }
 
-#if !os(iOS)
     func testCreateTlsContextWithFilePath() throws{
+        try skipIfiOS();
         let cert_path = try GetEnvironmentVarOrSkip(environmentVarName: "AWS_TEST_MQTT311_IOT_CORE_X509_CERT")
         let private_key_path = try GetEnvironmentVarOrSkip(environmentVarName: "AWS_TEST_MQTT311_IOT_CORE_X509_KEY")
         let options = try TLSContextOptions.makeMtlsFromFilePath(certificatePath: cert_path!, privateKeyPath: private_key_path!)
         let context = try TLSContext(options: options, mode: .client)
         _ = TLSConnectionOptions(context: context)
     }
-#endif
 }
