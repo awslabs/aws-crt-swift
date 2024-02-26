7e4dd9ee74f241e3a76f712359eea4e9297d81d1
Steve Kim <sbstevek@amazon.com>
2024-02-26 09:21:35 -0800

    ConnackPacket

--- Source/AwsCommonRuntimeKit/mqtt/Mqtt5.swift
+++ Source/AwsCommonRuntimeKit/mqtt/Mqtt5.swift
@@ -801,49 +801,54 @@ public class ConnackPacket {
     var reasonCode: ConnectReasonCode
 
     /// A time interval, in seconds, that the server will persist this connection's MQTT session state for.  If present, this value overrides any session expiry specified in the preceding CONNECT packet.
-    var sessionExpiryIntervalSec: Int
+    var sessionExpiryIntervalSec: Int?
 
     /// The maximum amount of in-flight QoS 1 or 2 messages that the server is willing to handle at once. If omitted or None, the limit is based on the valid MQTT packet id space (65535).
-    var receiveMaximum: Int
+    var receiveMaximum: Int?
 
     /// The maximum message delivery quality of service that the server will allow on this connection.
-    var maximumQos: QoS
+    var maximumQos: QoS?
 
     /// Indicates whether the server supports retained messages.  If None, retained messages are supported.
-    var retainAvailable: Bool
+    var retainAvailable: Bool?
 
     /// Specifies the maximum packet size, in bytes, that the server is willing to accept.  If None, there is no limit beyond what is imposed by the MQTT spec itself.
-    var maximumPacketSize: Int
+    var maximumPacketSize: Int?
 
     /// Specifies a client identifier assigned to this connection by the server.  Only valid when the client id of the preceding CONNECT packet was left empty.
-    var assignedClientIdentifier: String
+    var assignedClientIdentifier: String?
 
     /// The maximum allowed value for topic aliases in outbound publish packets.  If 0 or None, then outbound topic aliasing is not allowed.
-    var topicAliasMaximum: Int
+    var topicAliasMaximum: Int?
 
     /// Additional diagnostic information about the result of the connection attempt.
-    var reasonString: String
+    var reasonString: String?
 
     /// Array of MQTT5 user properties included with the packet.
-    var userProperties: [UserProperty]
+    var userProperties: [UserProperty]?
 
     /// Indicates whether the server supports wildcard subscriptions.  If None, wildcard subscriptions are supported.
-    var wildcardSubscriptionsAvailable: Bool
+    var wildcardSubscriptionsAvailable: Bool?
 
     /// Indicates whether the server supports subscription identifiers.  If None, subscription identifiers are supported.
-    var subscriptionIdentifiersAvailable: Bool
+    var subscriptionIdentifiersAvailable: Bool?
 
     /// Indicates whether the server supports shared subscription topic filters.  If None, shared subscriptions are supported.
-    var sharedSubscriptionAvailable: Bool
+    var sharedSubscriptionAvailable: Bool?
 
     /// Server-requested override of the keep alive interval, in seconds.  If None, the keep alive value sent by the client should be used.
-    var serverKeepAliveSec: Int
+    var serverKeepAliveSec: Int?
 
     /// A value that can be used in the creation of a response topic associated with this connection. MQTT5-based request/response is outside the purview of the MQTT5 spec and this client.
-    var responseInformation: String
+    var responseInformation: String?
 
     /// Property indicating an alternate server that the client may temporarily or permanently attempt to connect to instead of the configured endpoint.  Will only be set if the reason code indicates another server may be used (ServerMoved, UseAnotherServer).
-    var serverReference: String
+    var serverReference: String?
+
+    init (sessionPresent: Bool, reasonCode: ConnectReasonCode) {
+        self.sessionPresent = sessionPresent
+        self.reasonCode = reasonCode
+    }
 }
 
 /// Configuration for the creation of MQTT5 clients
