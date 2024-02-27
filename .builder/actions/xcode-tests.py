aae541ce9f737c230320892c5afe9368fc91c2af
Zhihui Xia <zhvxia@amazon.com>
2024-02-27 13:59:56 -0800

    use string literal for commands...

--- .builder/actions/xcode-tests.py
+++ .builder/actions/xcode-tests.py
@@ -5,7 +5,6 @@ import sys
 class XCodeTests(Builder.Action):
     def run(self, env):
         destination = env.shell.getenv("XCODE_DESTINATION")
-        platform_string = '\'platform='+destination+'\''
         commands =[
             'sudo',
             'xcodebuild',
@@ -13,18 +12,6 @@ class XCodeTests(Builder.Action):
             'aws-crt-swift-Package',
             'test',
             '-destination',
-            platform_string
+            str('\'platform='+destination+'\'')
         ]
-        print(commands)
-        print(platform_string.strip('\"'))
-        commands2 =[
-            'sudo',
-            'xcodebuild',
-            '-scheme',
-            'aws-crt-swift-Package',
-            'test',
-            '-destination',
-            platform_string.strip('\"')
-        ]
-        print(commands2)
-        env.shell.exec(commands2, check=True)
+        env.shell.exec(commands, check=True)
