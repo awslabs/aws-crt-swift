458946f94047d7dc95f74d9499a7e8599a03ff17
Zhihui Xia <zhvxia@amazon.com>
2024-02-29 12:56:54 -0800

    update uname

--- integ_test.sh
+++ integ_test.sh
@@ -2,7 +2,8 @@
 curl -L -o /tmp/http_client_test.py https://raw.githubusercontent.com/awslabs/aws-c-http/main/integration-testing/http_client_test.py
 ARCH=$(uname -a)
 ARCH_STRING="arm64"
-if [[ "$ARCH" == x86_64* ]]; then
+echo $ARCH
+if [[ "$ARCH" == *x86_64* ]]; then
   ARCH_STRING="x86_64"
 fi
 python3 /tmp/http_client_test.py .build/$ARCH_STRING-apple-macosx/release/Elasticurl
