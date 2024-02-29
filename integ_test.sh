2f7a372f3997856d91b64ff511e169114b31d95a
Zhihui Xia <zhvxia@amazon.com>
2024-02-29 14:03:57 -0800

    fix param name

--- integ_test.sh
+++ integ_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -L -o /tmp/http_client_test.py https://raw.githubusercontent.com/awslabs/aws-c-http/main/integration-testing/http_client_test.py
 ARCH=$(uname -a | sed 's/.* \([^ ]*\)$/\1/')
-python3 /tmp/http_client_test.py .build/$ARCH_STRING-apple-macosx/release/Elasticurl
+python3 /tmp/http_client_test.py .build/$ARCH-apple-macosx/release/Elasticurl
