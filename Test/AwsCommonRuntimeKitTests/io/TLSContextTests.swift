4f4b628427e42d79845b3a053733ff087e4284bc
Zhihui Xia <zhvxia@amazon.com>
2024-04-02 09:06:32 -0700

    disable test

--- Test/AwsCommonRuntimeKitTests/io/TLSContextTests.swift
+++ Test/AwsCommonRuntimeKitTests/io/TLSContextTests.swift
@@ -13,16 +13,16 @@ class TLSContextTests: XCBaseTestCase {
 
 //    TODO: The test is disabled as the github CI failed on access default keychain.
 //    TODO: Add test for testCreateTlsContextWithRawData()
-  func testCreateTlsContextWithFilePath() throws{
-      try skipIfiOS()
-      try skipIftvOS()
-      try skipIfwatchOS()
+//    func testCreateTlsContextWithFilePath() throws{
+//       try skipIfiOS()
+//       try skipIftvOS()
+//       try skipIfwatchOS()
 
-      let cert_path = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT311_IOT_CORE_X509_CERT")
-      let private_key_path = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT311_IOT_CORE_X509_KEY")
-      let options = try TLSContextOptions.makeMTLS(certificatePath: cert_path, privateKeyPath: private_key_path)
+//       let cert_path = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT311_IOT_CORE_X509_CERT")
+//       let private_key_path = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT311_IOT_CORE_X509_KEY")
+//       let options = try TLSContextOptions.makeMTLS(certificatePath: cert_path, privateKeyPath: private_key_path)
 
-      let context = try TLSContext(options: options, mode: .client)
-      _ = TLSConnectionOptions(context: context)
-  }
+//       let context = try TLSContext(options: options, mode: .client)
+//       _ = TLSConnectionOptions(context: context)
+//    }
 }
