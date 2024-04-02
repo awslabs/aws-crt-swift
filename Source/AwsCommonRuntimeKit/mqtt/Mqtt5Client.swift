e1360a479d49776e1b10d3d6c1c9f8b097cdac27
Zhihui Xia <zhvxia@amazon.com>
2024-04-02 09:55:50 -0700

    setup for close function

--- Source/AwsCommonRuntimeKit/mqtt/Mqtt5Client.swift
+++ Source/AwsCommonRuntimeKit/mqtt/Mqtt5Client.swift
@@ -6,12 +6,12 @@ import AwsCMqtt
 import AwsCIo
 
 public class Mqtt5Client {
-     private var rawValue: UnsafeMutablePointer<aws_mqtt5_client>
+     private var rawValue: UnsafeMutablePointer<aws_mqtt5_client>?
      private let clientOptions: MqttClientOptions
 
     init(clientOptions options: MqttClientOptions) throws {
         self.clientOptions = options
-        
+
         guard let rawValue = (options.withCPointer { optionsPointer in
                 return aws_mqtt5_client_new(allocator.rawValue, optionsPointer)
         })  else {
@@ -24,8 +24,8 @@ public class Mqtt5Client {
         aws_mqtt5_client_release(rawValue)
     }
 
-    /// TODO: Discard all client operations and force releasing the client. The client could not perform any operation after calling this function.
     public func close() {
-        // TODO
+        aws_mqtt5_client_release(rawValue)
+        rawValue = nil
     }
 }
