7a9be3a9db0f539bdcf0113043cf97d51afbdd23
Steve Kim <sbstevek@amazon.com>
2024-04-05 15:05:17 -0700

    initial connect test framework

--- Test/AwsCommonRuntimeKitTests/mqtt/Mqtt5ClientTests.swift
+++ Test/AwsCommonRuntimeKitTests/mqtt/Mqtt5ClientTests.swift
@@ -32,7 +32,9 @@ func onLifecycleEventDisconnectionMinimal(_ : LifecycleDisconnectData){
 
 
 class Mqtt5ClientTests: XCBaseTestCase {
-
+    /*===============================================================
+                     CREATION TEST CASES
+    =================================================================*/
     /*
      * [New-UC1] Happy path. Minimal creation and cleanup
      */
@@ -115,5 +117,165 @@ class Mqtt5ClientTests: XCBaseTestCase {
         XCTAssertNotNil(clientOptions)
         let mqtt5client = try Mqtt5Client(clientOptions: clientOptions);
         XCTAssertNotNil(mqtt5client)
+    }
+
+    /*===============================================================
+                     DIRECT CONNECT TEST CASES
+    =================================================================*/
+
+    class MqttTestContext {
+        public var onPublishReceived: OnPublishReceived?
+        public var onLifecycleEventStopped: OnLifecycleEventStopped?
+        public var onLifecycleEventAttemptingConnect: OnLifecycleEventAttemptingConnect?
+        public var onLifecycleEventConnectionSuccess: OnLifecycleEventConnectionSuccess?
+        public var onLifecycleEventConnectionFailure: OnLifecycleEventConnectionFailure?
+        public var onLifecycleEventDisconnection: OnLifecycleEventDisconnection?
+
+        public let semaphoreConnectionSuccess: DispatchSemaphore
+        public let semaphoreConnectionFailure: DispatchSemaphore
+        public let semaphoreDisconnection: DispatchSemaphore
+        public let semaphoreStopped: DispatchSemaphore
+
+        init(onPublishReceived: OnPublishReceived? = nil,
+             onLifecycleEventStopped: OnLifecycleEventStopped? = nil,
+             onLifecycleEventAttemptingConnect: OnLifecycleEventAttemptingConnect? = nil,
+             onLifecycleEventConnectionSuccess: OnLifecycleEventConnectionSuccess? = nil,
+             onLifecycleEventConnectionFailure: OnLifecycleEventConnectionFailure? = nil,
+             onLifecycleEventDisconnection: OnLifecycleEventDisconnection? = nil) {
+
+            self.semaphoreConnectionSuccess = DispatchSemaphore(value: 0)
+            self.semaphoreConnectionFailure = DispatchSemaphore(value: 0)
+            self.semaphoreDisconnection = DispatchSemaphore(value: 0)
+            self.semaphoreStopped = DispatchSemaphore(value: 0)
+
+            self.onPublishReceived = onPublishReceived
+            self.onLifecycleEventStopped = onLifecycleEventStopped
+            self.onLifecycleEventAttemptingConnect = onLifecycleEventAttemptingConnect
+            self.onLifecycleEventConnectionSuccess = onLifecycleEventConnectionSuccess
+            self.onLifecycleEventConnectionFailure = onLifecycleEventConnectionFailure
+            self.onLifecycleEventDisconnection = onLifecycleEventDisconnection
+
+            self.onPublishReceived = onPublishReceived ?? { _ in
+                print("Mqtt5ClientTests: onPublishReceived")
+            }
+            self.onLifecycleEventStopped = onLifecycleEventStopped ?? { _ in
+                print("Mqtt5ClientTests: onLifecycleEventStopped")
+                self.semaphoreStopped.signal()
+            }
+            self.onLifecycleEventAttemptingConnect = onLifecycleEventAttemptingConnect ?? { _ in
+                print("Mqtt5ClientTests: onLifecycleEventAttemptingConnect")
+            }
+            self.onLifecycleEventConnectionSuccess = onLifecycleEventConnectionSuccess ?? { _ in
+                print("Mqtt5ClientTests: onLifecycleEventConnectionSuccess")
+                self.semaphoreConnectionSuccess.signal()
+            }
+            self.onLifecycleEventConnectionFailure = onLifecycleEventConnectionFailure ?? { _ in
+                print("Mqtt5ClientTests: onLifecycleEventConnectionFailure")
+                self.semaphoreConnectionFailure.signal()
+            }
+            self.onLifecycleEventDisconnection = onLifecycleEventDisconnection ?? { _ in
+                print("Mqtt5ClientTests: onLifecycleEventDisconnection")
+                self.semaphoreDisconnection.signal()
+            }
+         }
+    }
+
+    func createClient(clientOptions: MqttClientOptions?, testContext: MqttTestContext) throws -> Mqtt5Client {
+
+        let clientOptionsWithCallbacks: MqttClientOptions
+
+        if let clientOptions = clientOptions {
+            clientOptionsWithCallbacks = MqttClientOptions(
+                hostName: clientOptions.hostName,
+                port: clientOptions.port,
+                bootstrap: clientOptions.bootstrap,
+                socketOptions: clientOptions.socketOptions,
+                tlsCtx: clientOptions.tlsCtx,
+                httpProxyOptions: clientOptions.httpProxyOptions,
+                connectOptions: clientOptions.connectOptions,
+                sessionBehavior: clientOptions.sessionBehavior,
+                extendedValidationAndFlowControlOptions: clientOptions.extendedValidationAndFlowControlOptions,
+                offlineQueueBehavior: clientOptions.offlineQueueBehavior,
+                retryJitterMode: clientOptions.retryJitterMode,
+                minReconnectDelay: clientOptions.minReconnectDelay,
+                maxReconnectDelay: clientOptions.maxReconnectDelay,
+                minConnectedTimeToResetReconnectDelay: clientOptions.minConnectedTimeToResetReconnectDelay,
+                pingTimeout: clientOptions.pingTimeout,
+                connackTimeout: clientOptions.connackTimeout,
+                ackTimeout: clientOptions.ackTimeout,
+                topicAliasingOptions: clientOptions.topicAliasingOptions,
+                onPublishReceivedFn: testContext.onPublishReceived,
+                onLifecycleEventStoppedFn: testContext.onLifecycleEventStopped,
+                onLifecycleEventAttemptingConnectFn: testContext.onLifecycleEventAttemptingConnect,
+                onLifecycleEventConnectionSuccessFn: testContext.onLifecycleEventConnectionSuccess,
+                onLifecycleEventConnectionFailureFn: testContext.onLifecycleEventConnectionFailure,
+                onLifecycleEventDisconnectionFn: testContext.onLifecycleEventDisconnection)
+        } else {
+            let elg = try EventLoopGroup()
+            let resolver = try HostResolver(eventLoopGroup: elg,
+                maxHosts: 8,
+                maxTTL: 30)
+            let clientBootstrap = try ClientBootstrap(
+                eventLoopGroup: elg,
+                hostResolver: resolver)
+            let socketOptions = SocketOptions()
+
+            clientOptionsWithCallbacks = MqttClientOptions(
+                hostName: "localhost",
+                port: 443,
+                bootstrap: clientBootstrap,
+                socketOptions: socketOptions,
+                onPublishReceivedFn: testContext.onPublishReceived,
+                onLifecycleEventStoppedFn: testContext.onLifecycleEventStopped,
+                onLifecycleEventAttemptingConnectFn: testContext.onLifecycleEventAttemptingConnect,
+                onLifecycleEventConnectionSuccessFn: testContext.onLifecycleEventConnectionSuccess,
+                onLifecycleEventConnectionFailureFn: testContext.onLifecycleEventConnectionFailure,
+                onLifecycleEventDisconnectionFn: testContext.onLifecycleEventDisconnection)
+        }
+
+        let mqtt5Client = try Mqtt5Client(clientOptions: clientOptionsWithCallbacks)
+        XCTAssertNotNil(mqtt5Client)
+        return mqtt5Client
+    }
+
+    /*
+     * [ConnDC-UC1] Happy path
+     */
+
+    func testMqtt5DirectConnectMinimum() throws {
+        let directHost = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_DIRECT_MQTT_HOST")
+        let directPort = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_DIRECT_MQTT_PORT")
+        print("directHost:\(directHost)")
+        print("directPort:\(directPort)")
+        
+        let elg = try EventLoopGroup()
+        let resolver = try HostResolver.makeDefault(eventLoopGroup: elg)
+        let clientBootstrap = try ClientBootstrap(eventLoopGroup: elg, hostResolver: resolver)
+        let socketOptions = SocketOptions()
+        let connectOptions = MqttConnectOptions()
+        let clientOptions = MqttClientOptions(
+            hostName: directHost,
+            port: UInt32(directPort)!,
+            bootstrap: clientBootstrap,
+            socketOptions: socketOptions)
+    
+        let testContext = MqttTestContext()
+        let client = try createClient(clientOptions: clientOptions, testContext: testContext)
+        try client.start()
+        if testContext.semaphoreConnectionSuccess.wait(timeout: .now() + 5) == .timedOut {
+            print("Connection Success Timed out after 5 seconds")
+            XCTFail("Connection Timed Out")
+        }
+        
+        try client.stop()
+        if testContext.semaphoreDisconnection.wait(timeout: .now() + 5) == .timedOut {
+            print("Disconnection timed out after 5 seconds")
+            XCTFail("Disconnection timed out")
+        }
+        
+        if testContext.semaphoreStopped.wait(timeout: .now() + 5) == .timedOut {
+            print("Stop timed out after 5 seconds")
+            XCTFail("Stop timed out")
+        }
     }
 }
