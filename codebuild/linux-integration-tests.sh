96e5fbe5e4cd7e12d341269a7b81d96d22c53f36
Zhihui Xia <zhvxia@amazon.com>
2024-04-08 10:44:45 -0700

    fix package name

--- codebuild/linux-integration-tests.sh
+++ codebuild/linux-integration-tests.sh
@@ -10,4 +10,4 @@ env
 
 swift -v
 python3 -c "from urllib.request import urlretrieve; urlretrieve('$BUILDER_HOST/$BUILDER_SOURCE/$BUILDER_VERSION/builder.pyz?run=$CODEBUILD_BUILD_ID', 'builder.pyz')"
-python3 builder.pyz build --project aws-crt-cpp downstream --cmake-extra=-DUSE_OPENSSL=ON
+python3 builder.pyz build --project $PACKAGE_NAME downstream --cmake-extra=-DUSE_OPENSSL=ON
