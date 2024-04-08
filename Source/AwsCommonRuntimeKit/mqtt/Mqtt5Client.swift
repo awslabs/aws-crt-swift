6e98fb93557e731c63650bfff66a07e8768c419c
Zhihui Xia <zhvxia@amazon.com>
2024-04-08 14:42:18 -0700

    remove common error

--- Source/AwsCommonRuntimeKit/mqtt/Mqtt5Client.swift
+++ Source/AwsCommonRuntimeKit/mqtt/Mqtt5Client.swift
@@ -7,11 +7,8 @@ import AwsCIo
 
 public class Mqtt5Client {
      private var rawValue: UnsafeMutablePointer<aws_mqtt5_client>?
-     private let clientOptions: MqttClientOptions
 
     init(clientOptions options: MqttClientOptions) throws {
-        self.clientOptions = options
-
         guard let rawValue = (options.withCPointer { optionsPointer in
                 return aws_mqtt5_client_new(allocator.rawValue, optionsPointer)
         })  else {
