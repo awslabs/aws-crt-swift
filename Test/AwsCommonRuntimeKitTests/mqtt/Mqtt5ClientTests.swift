3c60e83c26c050f7206f8bfe713fc767f9c8ecba
Steve Kim <sbstevek@amazon.com>
2024-04-17 13:56:14 -0700

    rearrange test funcs

--- Test/AwsCommonRuntimeKitTests/mqtt/Mqtt5ClientTests.swift
+++ Test/AwsCommonRuntimeKitTests/mqtt/Mqtt5ClientTests.swift
@@ -32,100 +32,10 @@ func onLifecycleEventDisconnectionMinimal(_ : LifecycleDisconnectData){
 
 
 class Mqtt5ClientTests: XCBaseTestCase {
-    /*===============================================================
-                     CREATION TEST CASES
-    =================================================================*/
-    /*
-     * [New-UC1] Happy path. Minimal creation and cleanup
-     */
-    func testMqtt5ClientNewMinimal() throws {
-        let elg = try EventLoopGroup()
-        let resolver = try HostResolver(eventLoopGroup: elg,
-                maxHosts: 8,
-                maxTTL: 30)
-
-        let clientBootstrap = try ClientBootstrap(eventLoopGroup: elg,
-                hostResolver: resolver)
-        XCTAssertNotNil(clientBootstrap)
-        let socketOptions = SocketOptions()
-        XCTAssertNotNil(socketOptions)
-        let clientOptions = MqttClientOptions(hostName: "localhost", port: 1883, bootstrap: clientBootstrap,
-                                   socketOptions: socketOptions);
-        XCTAssertNotNil(clientOptions)
-        let mqtt5client = try Mqtt5Client(clientOptions: clientOptions);
-        XCTAssertNotNil(mqtt5client)
-
-    }
-
-    /*
-     * [New-UC2] Maximum creation and cleanup
-     */
-    func testMqtt5ClientNewFull() throws {
-        let elg = try EventLoopGroup()
-        let resolver = try HostResolver(eventLoopGroup: elg,
-                maxHosts: 8,
-                maxTTL: 30)
-
-        let clientBootstrap = try ClientBootstrap(eventLoopGroup: elg,
-                hostResolver: resolver)
-        XCTAssertNotNil(clientBootstrap)
-        let socketOptions = SocketOptions()
-        XCTAssertNotNil(socketOptions)
-        let tlsOptions = TLSContextOptions()
-        let tlsContext = try TLSContext(options: tlsOptions, mode: .client)
-        let will = PublishPacket(qos: QoS.atLeastOnce, topic: "test/Mqtt5_Binding_SWIFT/testMqtt5ClientNewFull",
-                                 payload: "will test".data(using: .utf8))
-
-        let uuid = UUID().uuidString
-        let connectOptions = MqttConnectOptions(
-                            keepAliveInterval: 30,
-                            clientId: "testMqtt5ClientNewFull_" + uuid,
-                            sessionExpiryInterval: 1000,
-                            requestResponseInformation: true,
-                            requestProblemInformation: true,
-                            receiveMaximum: 1000,
-                            maximumPacketSize: 1000,
-                            willDelayInterval: 1000,
-                            will: will,
-                            userProperties:   [UserProperty(name: "name1",value: "value1"),
-                                               UserProperty(name: "name2",value: "value2"),
-                                               UserProperty(name: "name3",value: "value3")])
-
-
-        let clientOptions = MqttClientOptions( hostName: "localhost",
-                                            port: 1883,
-                                            bootstrap: clientBootstrap,
-                                            socketOptions: socketOptions,
-                                            tlsCtx: tlsContext,
-                                            connectOptions: connectOptions,
-                                            sessionBehavior: ClientSessionBehaviorType.clean,
-                                            extendedValidationAndFlowControlOptions: ExtendedValidationAndFlowControlOptions.awsIotCoreDefaults,
-                                            offlineQueueBehavior: ClientOperationQueueBehaviorType.failAllOnDisconnect,
-                                            retryJitterMode: ExponentialBackoffJitterMode.full,
-                                            minReconnectDelay: 1000,
-                                            maxReconnectDelay: 1000,
-                                            minConnectedTimeToResetReconnectDelay: 1000,
-                                            pingTimeout: 10,
-                                            connackTimeout: 10,
-                                            ackTimeout: 60,
-                                            topicAliasingOptions: TopicAliasingOptions(),
-                                            onPublishReceivedFn: onPublishReceivedCallbackMinimal,
-                                            onLifecycleEventStoppedFn: onLifecycleEventStoppedMinimal,
-                                            onLifecycleEventAttemptingConnectFn: onLifecycleEventAttemptingConnectMinimal,
-                                            onLifecycleEventConnectionFailureFn: onLifecycleEventConnectionFailureMinimal,
-                                            onLifecycleEventDisconnectionFn: onLifecycleEventDisconnectionMinimal)
-        XCTAssertNotNil(clientOptions)
-        let mqtt5client = try Mqtt5Client(clientOptions: clientOptions);
-        XCTAssertNotNil(mqtt5client)
-    }
 
     func createClientId() -> String {
         return "aws-crt-swift-unit-test-" + UUID().uuidString
     }
-
-    /*===============================================================
-                     DIRECT CONNECT TEST CASES
-    =================================================================*/
 
     class MqttTestContext {
         public var contextName: String
@@ -252,6 +162,96 @@ class Mqtt5ClientTests: XCBaseTestCase {
         return mqtt5Client
     }
 
+    /*===============================================================
+                     CREATION TEST CASES
+    =================================================================*/
+    /*
+     * [New-UC1] Happy path. Minimal creation and cleanup
+     */
+    func testMqtt5ClientNewMinimal() throws {
+        let elg = try EventLoopGroup()
+        let resolver = try HostResolver(eventLoopGroup: elg,
+                maxHosts: 8,
+                maxTTL: 30)
+
+        let clientBootstrap = try ClientBootstrap(eventLoopGroup: elg,
+                hostResolver: resolver)
+        XCTAssertNotNil(clientBootstrap)
+        let socketOptions = SocketOptions()
+        XCTAssertNotNil(socketOptions)
+        let clientOptions = MqttClientOptions(hostName: "localhost", port: 1883, bootstrap: clientBootstrap,
+                                   socketOptions: socketOptions);
+        XCTAssertNotNil(clientOptions)
+        let mqtt5client = try Mqtt5Client(clientOptions: clientOptions);
+        XCTAssertNotNil(mqtt5client)
+
+    }
+
+    /*
+     * [New-UC2] Maximum creation and cleanup
+     */
+    func testMqtt5ClientNewFull() throws {
+        let elg = try EventLoopGroup()
+        let resolver = try HostResolver(eventLoopGroup: elg,
+                maxHosts: 8,
+                maxTTL: 30)
+
+        let clientBootstrap = try ClientBootstrap(eventLoopGroup: elg,
+                hostResolver: resolver)
+        XCTAssertNotNil(clientBootstrap)
+        let socketOptions = SocketOptions()
+        XCTAssertNotNil(socketOptions)
+        let tlsOptions = TLSContextOptions()
+        let tlsContext = try TLSContext(options: tlsOptions, mode: .client)
+        let will = PublishPacket(qos: QoS.atLeastOnce, topic: "test/Mqtt5_Binding_SWIFT/testMqtt5ClientNewFull",
+                                 payload: "will test".data(using: .utf8))
+
+        let uuid = UUID().uuidString
+        let connectOptions = MqttConnectOptions(
+                            keepAliveInterval: 30,
+                            clientId: "testMqtt5ClientNewFull_" + uuid,
+                            sessionExpiryInterval: 1000,
+                            requestResponseInformation: true,
+                            requestProblemInformation: true,
+                            receiveMaximum: 1000,
+                            maximumPacketSize: 1000,
+                            willDelayInterval: 1000,
+                            will: will,
+                            userProperties:   [UserProperty(name: "name1",value: "value1"),
+                                               UserProperty(name: "name2",value: "value2"),
+                                               UserProperty(name: "name3",value: "value3")])
+
+
+        let clientOptions = MqttClientOptions( hostName: "localhost",
+                                            port: 1883,
+                                            bootstrap: clientBootstrap,
+                                            socketOptions: socketOptions,
+                                            tlsCtx: tlsContext,
+                                            connectOptions: connectOptions,
+                                            sessionBehavior: ClientSessionBehaviorType.clean,
+                                            extendedValidationAndFlowControlOptions: ExtendedValidationAndFlowControlOptions.awsIotCoreDefaults,
+                                            offlineQueueBehavior: ClientOperationQueueBehaviorType.failAllOnDisconnect,
+                                            retryJitterMode: ExponentialBackoffJitterMode.full,
+                                            minReconnectDelay: 1000,
+                                            maxReconnectDelay: 1000,
+                                            minConnectedTimeToResetReconnectDelay: 1000,
+                                            pingTimeout: 10,
+                                            connackTimeout: 10,
+                                            ackTimeout: 60,
+                                            topicAliasingOptions: TopicAliasingOptions(),
+                                            onPublishReceivedFn: onPublishReceivedCallbackMinimal,
+                                            onLifecycleEventStoppedFn: onLifecycleEventStoppedMinimal,
+                                            onLifecycleEventAttemptingConnectFn: onLifecycleEventAttemptingConnectMinimal,
+                                            onLifecycleEventConnectionFailureFn: onLifecycleEventConnectionFailureMinimal,
+                                            onLifecycleEventDisconnectionFn: onLifecycleEventDisconnectionMinimal)
+        XCTAssertNotNil(clientOptions)
+        let mqtt5client = try Mqtt5Client(clientOptions: clientOptions);
+        XCTAssertNotNil(mqtt5client)
+    }
+
+    /*===============================================================
+                     DIRECT CONNECT TEST CASES
+    =================================================================*/
     /*
      * [ConnDC-UC1] Happy path
      */
