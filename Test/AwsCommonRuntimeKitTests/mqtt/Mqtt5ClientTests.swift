ec209461e020c506cb1546b36084b3fe821d4a69
Steve Kim <sbstevek@amazon.com>
2024-04-18 09:34:32 -0700

    try

--- Test/AwsCommonRuntimeKitTests/mqtt/Mqtt5ClientTests.swift
+++ Test/AwsCommonRuntimeKitTests/mqtt/Mqtt5ClientTests.swift
@@ -378,7 +378,7 @@ class Mqtt5ClientTests: XCBaseTestCase {
      * [ConnDC-UC4] Direct Connection with mutual TLS
      */
     func testMqtt5DirectConnectWithMutualTLS() throws {
-        skipUnsupportedPlatforms()
+        try skipUnsupportedPlatforms()
         let inputHost = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_HOST")
         let inputCert = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_RSA_CERT")
         let inputKey = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_RSA_KEY")
@@ -690,7 +690,7 @@ class Mqtt5ClientTests: XCBaseTestCase {
     * [ConnNegativeID-UC7] Double Client ID Failure test
     */
     func testMqtt5MTLSConnectDoubleClientIdFailure() throws {
-        skipUnsupportedPlatforms()
+        try skipUnsupportedPlatforms()
         let inputHost = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_HOST")
         let inputCert = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_RSA_CERT")
         let inputKey = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_RSA_KEY")
@@ -867,7 +867,7 @@ class Mqtt5ClientTests: XCBaseTestCase {
     * [Negotiated-UC3] server settings limit test
     */
     func testMqtt5NegotiatedSettingsServerLimit() throws {
-        skipUnsupportedPlatforms()
+        try skipUnsupportedPlatforms()
         let inputHost = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_HOST")
         let inputCert = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_RSA_CERT")
         let inputKey = try getEnvironmentVarOrSkipTest(environmentVarName: "AWS_TEST_MQTT5_IOT_CORE_RSA_KEY")
