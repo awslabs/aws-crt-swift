4e473780133555a165204a2acc507ebee9fc4e65
Zhihui Xia <zhvxia@amazon.com>
2024-04-28 16:12:39 -0700

    fix error code

--- Source/AwsCommonRuntimeKit/mqtt/Mqtt5Client.swift
+++ Source/AwsCommonRuntimeKit/mqtt/Mqtt5Client.swift
@@ -83,11 +83,11 @@ public class Mqtt5Client {
                 errorCode: Int32,
                 userData: UnsafeMutableRawPointer?) {
                 let continuationCore = Unmanaged<ContinuationCore<SubackPacket>>.fromOpaque(userData!).takeRetainedValue()
-                
+
                 guard errorCode == AWS_OP_SUCCESS else {
                     return continuationCore.continuation.resume(throwing: CommonRunTimeError.crtError(CRTError(code: errorCode)))
                 }
-                                        
+
                 guard let suback = SubackPacket.convertFromNative(subackPacket)
                 else { fatalError("Suback missing in the subscription completion callback.") }
 
@@ -100,7 +100,7 @@ public class Mqtt5Client {
                 callbackOptions.completion_callback = subscribeCompletionCallback
                 callbackOptions.completion_user_data = ContinuationCore(continuation: continuation).passRetained()
                 let result = aws_mqtt5_client_subscribe(rawValue, subscribePacketPointer, &callbackOptions)
-                guard result == 0 else {
+                guard result == AWS_OP_SUCCESS else {
                     return continuation.resume(throwing: CommonRunTimeError.crtError(CRTError.makeFromLastError()))
                 }
             }
