3acbe94b63236e6a7b854dd92eb469d14dfda671
Zhihui Xia <zhvxia@amazon.com>
2024-07-15 09:28:32 -0700

    ios13 quick test

--- Test/IntegrationTests/MqttClient/MqttClient/MqttClientApp.swift
+++ Test/IntegrationTests/MqttClient/MqttClient/MqttClientApp.swift
@@ -3,12 +3,44 @@
 
 import SwiftUI
 
-@available(iOS 14.0, *)
+
 @main
+struct MqttClientAppWrapper {
+    static func main() {
+        if #available(iOS 14.0, *) {
+            MqttClientApp.main()
+        }
+        else {
+            // The main actor only exists for 14.0 and above.
+            UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(SceneDelegate.self))
+        }
+    }
+}
+
+
+@available(iOS 14.0, *)
 struct MqttClientApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
         }
     }
 }
