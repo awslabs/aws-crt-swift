e9b80dafe4f213ac699b21033bdd5629c7578012
DmitriyMusatkin <musatkd@amazon.com>
2024-08-02 13:31:59 -0700

    lint

--- Source/AwsCommonRuntimeKit/crt/Utilities.swift
+++ Source/AwsCommonRuntimeKit/crt/Utilities.swift
@@ -273,9 +273,7 @@ func withByteCursorFromStrings<Result>(
     }
 }
 
-public func scan<
-    S : Sequence, U
-    >(_ seq: S, _ initial: U, _ combine: (U, S.Element) -> U) -> [U] {
+public func scan<S: Sequence, U>(_ seq: S, _ initial: U, _ combine: (U, S.Element) -> U) -> [U] {
     var result: [U] = []
     result.reserveCapacity(seq.underestimatedCount)
     var runningResult = initial
@@ -300,8 +298,7 @@ public func withArrayOfCStrings<R>(
         argsBuffer.append(contentsOf: arg.utf8)
         argsBuffer.append(0)
     }
-    return argsBuffer.withUnsafeMutableBufferPointer {
-        (argsBuffer) in
+    return argsBuffer.withUnsafeMutableBufferPointer { (argsBuffer) in
         let ptr = UnsafeMutableRawPointer(argsBuffer.baseAddress!).bindMemory(
         to: CChar.self, capacity: argsBuffer.count)
         var cStrings: [UnsafeMutablePointer<CChar>?] = argsOffsets.map { ptr + $0 }
