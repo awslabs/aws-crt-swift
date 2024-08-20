2b95d259c20f2dac8fa422a9a115daaf0f229dfe
Waqar Ahmed Khan <waahm7@gmail.com>
2024-08-20 15:52:09 -0700

    fix bugs for Swift 6

--- aws-common-runtime/config/aws/common/config.h
+++ aws-common-runtime/config/aws/common/config.h
@@ -1,48 +1,19 @@
-#ifdef __APPLE__
-#    include <TargetConditionals.h>
-#endif
-
-#ifndef AWS_COMMON_CONFIG_H
-#    define AWS_COMMON_CONFIG_H
 /**
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+
+#ifndef AWS_COMMON_CONFIG_H
+#define AWS_COMMON_CONFIG_H
 /*
  * This header exposes compiler feature test results determined during cmake
  * configure time to inline function implementations. The macros defined here
  * should be considered to be an implementation detail, and can change at any
  * time.
  */
-#    if TARGET_OS_IPHONE
-#        define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#        define AWS_HAVE_GCC_INLINE_ASM
-/* #undef AWS_HAVE_MSVC_MULX */
-#        define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#    elif TARGET_OS_SIMULATOR
-#        define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#        define AWS_HAVE_GCC_INLINE_ASM
-/* #undef AWS_HAVE_MSVC_MULX */
-#        define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#    elif TARGET_OS_MAC
-#        define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#        define AWS_HAVE_GCC_INLINE_ASM
-/* #undef AWS_HAVE_MSVC_MULX */
-#        define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#    endif
-#else
-#    define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
-#    define AWS_HAVE_GCC_INLINE_ASM
-/* #undef AWS_HAVE_MSVC_MULX */
-#    define AWS_C_CAL_OPENSSLCRYPTO_COMMON_H
-#endif
+#define AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS
+#define AWS_HAVE_GCC_INLINE_ASM
 #define AWS_UNSTABLE_TESTING_API 1
-
 #define AWS_AFFINITY_METHOD 0
-
 #define AWS_HAVE_EXECINFO 1
-/* Disable Intel VTune tracing API here since aws-crt-swift doesn't use CMake */
-#define INTEL_NO_ITTNOTIFY_API
-
-/* Don't use APIs forbidden by App Stores (e.g. non-public system APIs) */
-#define AWS_APPSTORE_SAFE 1
+#endif
