3ac6ec01baa0baa0d027928f0d24e7e48f543c62
Vera Xia <zhvxia@amazon.com>
2024-12-30 16:29:04 -0800

    set use xcodebuild

--- .builder/actions/crt-ci-prep.py
+++ .builder/actions/crt-ci-prep.py
@@ -4,6 +4,6 @@ class CrtCiPrep(Builder.Action):
     def run(self, env):
         env.shell.setenv("TEST_RUNNER_AWS_TESTING_STS_ROLE_ARN", env.shell.get_secret("aws-c-auth-testing/sts-role-arn"))
         actions = [
-            Builder.SetupCrossCICrtEnvironment(is_xcodebuild=True)
+            Builder.SetupCrossCICrtEnvironment(use_xcodebuild=True)
         ]
         return Builder.Script(actions, name='crt-ci-prep')
