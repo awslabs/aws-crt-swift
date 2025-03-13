cdcbfad409afc5baa9ebb21b4baa3a6915ecdbcb
Vera Xia <zhvxia@amazon.com>
2025-03-13 13:46:13 -0700

    disable other unit tests

--- Test/AwsCommonRuntimeKitTests/auth/IMDSClientTests.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0.
-import XCTest
-@testable import AwsCommonRuntimeKit
-
-//TODO: write some tests
-class IMDSClientTest: XCBaseTestCase {
-
-}
