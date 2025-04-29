2beffec098770d75743c97a912288e8d19402ffd
Waqar Ahmed Khan <waahm7@gmail.com>
2025-04-29 10:14:55 -0700

    data.startIndex doesn't have to start with 0

--- Source/AwsCommonRuntimeKit/crt/ByteBuffer.swift
+++ Source/AwsCommonRuntimeKit/crt/ByteBuffer.swift
@@ -71,6 +71,6 @@ extension ByteBuffer: IStreamable {
     }
     
     public func isEndOfStream() -> Bool {
-        return self.currentIndex >= self.data.count
+        self.currentIndex >= self.data.endIndex
     }
 }
