97e9b47a26b835984002e92cb477eb08959f3ed9
Vera Xia <zhvxia@amazon.com>
2025-06-04 10:03:19 -0700

    remove default value for request response client options

--- Test/AwsCommonRuntimeKitTests/mqtt/MqttRRClientTests.swift
+++ Test/AwsCommonRuntimeKitTests/mqtt/MqttRRClientTests.swift
@@ -140,7 +140,10 @@ class Mqtt5RRClientTests: XCBaseTestCase {
   func testMqttRequestResponse_CreateDestroy() async throws {
     let testContext = MqttRRTestContext()
     let client = try createMqtt5Client(testContext: testContext)
-    let _ = try MqttRequestResponseClient.newFromMqtt5Client(mqtt5Client: client)
+    let _ = try MqttRequestResponseClient.newFromMqtt5Client(
+      mqtt5Client: client,
+      options: MqttRequestResponseClientOptions(
+        maxRequestResponseSubscription: 3, maxStreamingSubscription: 2, operationTimeout: 60))
   }
 
   func testMqttRequestResponse_GetNamedShadowSuccessRejected() throws {
