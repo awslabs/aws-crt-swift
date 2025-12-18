d8edde98a1093e79f0eda28daaec3da3b40b1f53
Steve Kim <sbstevek@amazon.com>
2025-12-18 09:47:42 -0800

    even older version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ var package = Package(
   dependencies: [
     // Arugment Parser Dependency for ElasticCurl
     .package(
-      url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
+      url: "https://github.com/apple/swift-argument-parser.git", exact: "1.0.3")
   ]
 )
 
